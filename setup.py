e7632a5f0e05f1b8ab4caf0e4f238485c64d2c9d
edeno <edeno@bu.edu>
2016-08-22 18:28:49 -0400

    Add testing code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='PFC-Hippocampus',
+    version='0.1.0.dev0',
+    description='Exploratory analysis of data from Jadhav 2016',
+    author='Eric Denovellis',
+    author_email='edeno@bu.edu',
+    packages=['src'],
+      )
---
+++ tests/test_data_filter.py
@@ -0,0 +1,73 @@
+import pytest
+import collections
+from unittest.mock import patch
+import numpy as np
+import src.data_filter as df
+
+
+def test_data_file_name_returns_correct_file():
+    Animal = collections.namedtuple('Animal', {'directory', 'short_name'})
+    animal = Animal(directory='test_dir', short_name='Test')
+    file_type = 'dummy'
+
+    day = 2
+    file_name = df.get_data_filename(animal, day, file_type)
+    assert '/Raw-Data/test_dir/Testdummy02.mat' in file_name
+
+    day = 11
+    file_name = df.get_data_filename(animal, day, file_type)
+    assert '/Raw-Data/test_dir/Testdummy11.mat' in file_name
+
+
+@pytest.mark.parametrize("search_array, target, expected_index", [
+    (np.arange(50, 150), 66, 16),
+    (np.arange(50, 150), 45, 0),
+    (np.arange(50, 150), 200, 99),
+    (np.arange(50, 150), 66.4, 16),
+    (np.arange(50, 150), 66.7, 17),
+    (np.arange(50, 150), [55, 65, 137], [5, 15, 87]),
+])
+def test_find_closest_ind(search_array, target, expected_index):
+    assert np.all(df.find_closest_ind(search_array, target) == expected_index)
+
+# Create a fake 'tasks' data set to test
+mock_data_struct = np.zeros(5, dtype={'names': ['type', 'environment'],
+                                      'formats': ['O', 'O']})
+mock_data_struct[0] = ('typeTest1', 'environTest1')
+mock_data_struct[1] = ('typeTest1', 'environTest2')
+mock_data_struct[2] = ('typeTest2', 'environTest2')
+mock_data_struct[3] = ('typeTest1', 'environTest2')
+mock_data_struct[4] = ('typeTest1', 'environTest1')
+
+mock_cell_array = {'task': np.array([[[], [mock_data_struct], []]])}
+
+
+@patch('scipy.io.loadmat')
+def test_get_epochs_single_day(mock_loadmat):
+    Animal = collections.namedtuple('Animal', {'directory', 'short_name'})
+    animal = Animal(directory='test_dir', short_name='Test')
+    days = 2
+    mock_loadmat.return_value = mock_cell_array
+
+    assert len(df.get_epochs(animal, days, epoch_type='', environment='')) == 5
+    assert len(df.get_epochs(animal, days, epoch_type='typeTest1', environment='')) == 4
+    assert len(df.get_epochs(animal, days, epoch_type='typeTest2', environment='')) == 1
+    assert len(df.get_epochs(animal, days, epoch_type='typeTest1', environment='environTest1')) == 2
+    assert len(df.get_epochs(animal, days, epoch_type='typeTest1', environment='environTest2')) == 2
+    assert len(df.get_epochs(animal, days, epoch_type='typeTest2', environment='environTest1')) == 0
+    assert len(df.get_epochs(animal, days, epoch_type='typeTest2', environment='environTest3')) == 0
+    assert len(df.get_epochs(animal, days, epoch_type='', environment='environTest2')) == 3
+
+
+mock_cell_array2 = {'task': np.array([[[], [mock_data_struct], [mock_data_struct]]])}
+
+
+@patch('scipy.io.loadmat')
+def test_get_epochs_multiple_days(mock_loadmat):
+    Animal = collections.namedtuple('Animal', {'directory', 'short_name'})
+    animal = Animal(directory='test_dir', short_name='Test')
+    days = [2, 3]
+    mock_loadmat.return_value = mock_cell_array2
+
+    assert len(df.get_epochs(animal, days, epoch_type='', environment='')) == 10
+    assert len(df.get_epochs(animal, days, epoch_type='typeTest1', environment='')) == 8
