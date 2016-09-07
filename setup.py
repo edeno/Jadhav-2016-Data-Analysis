07b46ca9dd19fdf1a42dcfca302d7aacc80d59d8
edeno <edeno@bu.edu>
2016-09-07 11:36:32 -0400

    Fix the name of the repo

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
 
 setup(
-    name='PFC-Hippocampus',
+    name='Jadhav-2016-Data-Analysis',
     version='0.1.0.dev0',
     description='Exploratory analysis of data from Jadhav 2016',
     author='Eric Denovellis',
