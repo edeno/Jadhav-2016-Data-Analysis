01e613222599ede768d1e306b8a1a5b78bf61c84
edeno <edeno@bu.edu>
2016-09-15 23:02:33 -0400

    Add setup requires and test requires

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@
 from setuptools import setup
 
 setup(
-    name='Jadhav-2016-Data-Analysis',
-    version='0.1.0.dev0',
-    description='Exploratory analysis of data from Jadhav 2016',
-    author='Eric Denovellis',
-    author_email='edeno@bu.edu',
-    packages=['src'],
+        name='Jadhav-2016-Data-Analysis',
+        version='0.1.0.dev0',
+        description='Exploratory analysis of data from Jadhav 2016',
+        author='Eric Denovellis',
+        author_email='edeno@bu.edu',
+        packages=['src'],
+        setup_requires=['pytest-runner'],
+        tests_require=['pytest']
       )
