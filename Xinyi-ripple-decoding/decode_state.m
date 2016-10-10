1bf711b0316d14fa18549861ba28b616d262f250
edeno <edeno@bu.edu>
2016-10-09 21:34:06 -0400

    Put spaces between variables and operators

--- Xinyi-ripple-decoding/decode_state.m
+++ Xinyi-ripple-decoding/decode_state.m
@@ -28,92 +28,100 @@ function [summary_statistic] = decode_state(pos, ...
     Lint_I_in ...
     )
 
-velocity=pos.data(:,5);
-%linVel=linpos{ex}{ep}.statematrix.linearVelocity;
+velocity = pos.data(:,5);
+
 for pic=1:length(rippleI)
-    rIndV=pic; %5, 12
-    rloc_Ind=find(position_time_stamps*1000>position_time_stamps_binned(ripple_index(rippleI(rIndV),1))&position_time_stamps*1000<position_time_stamps_binned(ripple_index(rippleI(rIndV),2)));
+    rIndV = pic; %5, 12
+    rloc_Ind = find(position_time_stamps * 1000 > position_time_stamps_binned(ripple_index(rippleI(rIndV), 1)) & ...
+        position_time_stamps * 1000 < position_time_stamps_binned(ripple_index(rippleI(rIndV), 2)));
     
-    rloc(pic)=vecLF(rloc_Ind(1),2);
-    vel(pic)=velocity(rloc_Ind(1),1);
+    rloc(pic) = vecLF(rloc_Ind(1),2);
+    vel(pic) = velocity(rloc_Ind(1),1);
 end
 
-velocity_threshold_index=find(vel<4);length(velocity_threshold_index)
+velocity_threshold_index = find(vel < 4);
 %only decode replay when the running speed < 4cm/sec
-ripplesconsN=traj_Ind(rippleI(velocity_threshold_index));
+ripplesconsN = traj_Ind(rippleI(velocity_threshold_index));
 
 %% decoder
 for pic=1:length(velocity_threshold_index)
     rIndV=velocity_threshold_index(pic); %5, 12
     
-    spike_tim=ripple_index(rippleI(rIndV),1):ripple_index(rippleI(rIndV),2); %from 1 to 90000~
+    spike_tim = ripple_index(rippleI(rIndV), 1):ripple_index(rippleI(rIndV), 2); %from 1 to 90000~
     numSteps=length(spike_tim);
-    xi=round(time/10);
+    xi=round(time / 10);
     
     %%
-    dt=1/33.4;spike_r=zeros(18,numSteps);
-    stateV_length=length(stateV);
-    numSteps=size(spike_r,2);
+    dt = 1 / 33.4;
+    spike_r = zeros(length(tetrode_number), numSteps);
+    stateV_length = length(stateV);
+    numSteps = size(spike_r,2);
     %P(x0|I);
-    Px_I_out=exp(-stateV.^2./(2*(2*stateV_delta)^2));
-    Px_I_out=Px_I_out./sum(Px_I_out);
-    Px_I_in=max(Px_I_out)*ones(1,stateV_length)-Px_I_out;
-    Px_I_in=Px_I_in./sum(Px_I_in);
-    Px_I0=Px_I_out;
-    Px_I1=Px_I_in;
-    Px_I2=Px_I_in;
-    Px_I3=Px_I_out;
+    Px_I_out = exp(-stateV.^2 ./ (2 * (2 * stateV_delta)^2));
+    Px_I_out = Px_I_out ./ sum(Px_I_out);
+    Px_I_in = max(Px_I_out) * ones(1,stateV_length) - Px_I_out;
+    Px_I_in = Px_I_in ./ sum(Px_I_in);
+    Px_I0 = Px_I_out;
+    Px_I1 = Px_I_in;
+    Px_I2 = Px_I_in;
+    Px_I3 = Px_I_out;
     %P(x0)=P(x0|I)P(I);
-    postx_I0=0.25*Px_I_out';
-    postx_I1=0.25*Px_I_in';
-    postx_I2=0.25*Px_I_in';
-    postx_I3=0.25*Px_I_out';
-    pI0_vec=zeros(numSteps,1);
-    pI1_vec=zeros(numSteps,1);
-    pI2_vec=zeros(numSteps,1);
-    pI3_vec=zeros(numSteps,1);
-    postxM_r_I0=zeros(stateV_length,numSteps);
-    postxM_r_I1=zeros(stateV_length,numSteps);
-    postxM_r_I2=zeros(stateV_length,numSteps);
-    postxM_r_I3=zeros(stateV_length,numSteps);
+    postx_I0 = 0.25 * Px_I_out';
+    postx_I1 = 0.25 * Px_I_in';
+    postx_I2 = 0.25 * Px_I_in';
+    postx_I3 = 0.25 * Px_I_out';
+    pI0_vec = zeros(numSteps, 1);
+    pI1_vec = zeros(numSteps, 1);
+    pI2_vec = zeros(numSteps, 1);
+    pI3_vec = zeros(numSteps, 1);
+    postxM_r_I0 = zeros(stateV_length, numSteps);
+    postxM_r_I1 = zeros(stateV_length, numSteps);
+    postxM_r_I2 = zeros(stateV_length, numSteps);
+    postxM_r_I3 = zeros(stateV_length, numSteps);
     %state transition
-    stateM_Indicator_outbound=stateM_I1_normalized_gaussian;
-    stateM_Indicator_inbound=stateM_Indicator0_normalized_gaussian;
-    stateM_I0=stateM_Indicator_outbound;
-    stateM_I1=stateM_Indicator_inbound;
-    stateM_I2=stateM_Indicator_inbound;
-    stateM_I3=stateM_Indicator_outbound;
+    stateM_Indicator_outbound = stateM_I1_normalized_gaussian;
+    stateM_Indicator_inbound = stateM_Indicator0_normalized_gaussian;
+    stateM_I0 = stateM_Indicator_outbound;
+    stateM_I1 = stateM_Indicator_inbound;
+    stateM_I2 = stateM_Indicator_inbound;
+    stateM_I3 = stateM_Indicator_outbound;
     
     for t=1:numSteps
-        tt=spike_tim(t);
-        aa=find(xi==position_time_stamps_binned(tt));
+        tt = spike_tim(t);
+        aa = find(xi == position_time_stamps_binned(tt));
         
-        onestep_I0=stateM_I0*postx_I0;
-        onestep_I1=stateM_I1*postx_I1;
-        onestep_I2=stateM_I2*postx_I2;
-        onestep_I3=stateM_I3*postx_I3;
+        onestep_I0 = stateM_I0*postx_I0;
+        onestep_I1 = stateM_I1*postx_I1;
+        onestep_I2 = stateM_I2*postx_I2;
+        onestep_I3 = stateM_I3*postx_I3;
         
-        L_I0=ones(stateV_length,1);L_I1=ones(stateV_length,1);L_I2=ones(stateV_length,1);L_I3=ones(stateV_length,1);
+        L_I0 = ones(stateV_length, 1);
+        L_I1 = ones(stateV_length, 1);
+        L_I2 = ones(stateV_length, 1);
+        L_I3 = ones(stateV_length, 1);
         
-        if isempty(aa)==1 %if no spike occurs at time t
-            L_I0=exp(-Lint_Indicator_outbound.*dt);L_I1=exp(-Lint_Indicator_outbound.*dt);
-            L_I2=exp(-Lint_Indicator_inbound.*dt);L_I3=exp(-Lint_Indicator_inbound.*dt);
+        if isempty(aa) %if no spike occurs at time t
+            L_I0=exp(-Lint_Indicator_outbound .* dt);
+            L_I1=exp(-Lint_Indicator_outbound .* dt);
+            L_I2=exp(-Lint_Indicator_inbound .* dt);
+            L_I3=exp(-Lint_Indicator_inbound .* dt);
             
-        elseif isempty(aa)==0 %if spikes
+        else %if spikes
             
-            l_out=zeros(stateV_length,length(aa));
+            l_out = zeros(stateV_length, length(aa));
             for j=1:length(aa)
-                jj=aa(j);
-                tetrode_ind=find(tet_ind(jj,:));
+                jj = aa(j);
+                tetrode_ind = find(tet_ind(jj,:));
                 spike_r(tetrode_ind, t) = 1;
                 l_out(:,j) = decode_per_tetrode(tet_sum(jj, tetrode_ind), ...
                     markAll{tetrode_ind}, procInd1_I_out{tetrode_ind}, procInd1_Ia_out{tetrode_ind}, ...
                     Xnum_I_out{tetrode_ind}, occ_Indicator_outbound, ...
                     Lint_I_out{tetrode_ind}, dt, smker);
             end
-            L_out=prod(l_out,2);L_out=L_out./sum(L_out);
+            L_out = prod(l_out,2);
+            L_out = L_out ./ sum(L_out);
             
-            l_in=zeros(stateV_length,length(aa));
+            l_in = zeros(stateV_length, length(aa));
             for j=1:length(aa)
                 jj=aa(j);
                 tetrode_ind=find(tet_ind(jj,:));
@@ -123,23 +131,30 @@ for pic=1:length(velocity_threshold_index)
                     Xnum_I_in{tetrode_ind}, occ_Indicator_inbound, ...
                     Lint_I_in{tetrode_ind}, dt, smker);
             end
-            L_in=prod(l_in,2);L_in=L_in./sum(L_in);
+            L_in = prod(l_in, 2);
+            L_in = L_in ./ sum(L_in);
             
-            L_I0=L_out;L_I1=L_out;L_I2=L_in;L_I3=L_in;
+            L_I0 = L_out;
+            L_I1 = L_out;
+            L_I2 = L_in;
+            L_I3 = L_in;
         end
         
-        totnorm=sum(onestep_I0.*L_I0)+sum(onestep_I1.*L_I1)+sum(onestep_I2.*L_I2)+sum(onestep_I3.*L_I3);
-        postx_I0=onestep_I0.*L_I0./totnorm;
-        postx_I1=onestep_I1.*L_I1./totnorm;
-        postx_I2=onestep_I2.*L_I2./totnorm;
-        postx_I3=onestep_I3.*L_I3./totnorm;
+        totnorm = sum(onestep_I0 .* L_I0) + ...
+            sum(onestep_I1 .* L_I1) + ...
+            sum(onestep_I2 .* L_I2) + ...
+            sum(onestep_I3 .* L_I3);
+        postx_I0 = onestep_I0 .* L_I0 ./ totnorm;
+        postx_I1 = onestep_I1 .* L_I1 ./ totnorm;
+        postx_I2 = onestep_I2 .* L_I2 ./ totnorm;
+        postx_I3 = onestep_I3 .* L_I3 ./ totnorm;
         
-        pI0_vec(t)=sum(postx_I0);
-        pI1_vec(t)=sum(postx_I1);
-        pI2_vec(t)=sum(postx_I2);
-        pI3_vec(t)=sum(postx_I3);
+        pI0_vec(t) = sum(postx_I0);
+        pI1_vec(t) = sum(postx_I1);
+        pI2_vec(t) = sum(postx_I2);
+        pI3_vec(t) = sum(postx_I3);
     end
     
-    summary_statistic{pic}=[pI0_vec pI1_vec pI2_vec pI3_vec];
+    summary_statistic{pic} = [pI0_vec pI1_vec pI2_vec pI3_vec];
 end
 end
