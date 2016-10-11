e30a72b09d8c8b8089c4f4556d07f3a3e088b4c1
edeno <edeno@bu.edu>
2016-10-11 16:02:34 -0400

    Rename mark0 to marks

--- Xinyi-ripple-decoding/kernel_density_model.m
+++ Xinyi-ripple-decoding/kernel_density_model.m
@@ -1,16 +1,16 @@
-function [markAll, spike_times, mark0, procInd1] = kernel_density_model(animal, day, tetrode_number, ...
+function [markAll, spike_times, marks, procInd1] = kernel_density_model(animal, day, tetrode_number, ...
     linear_position_time)
 mark_data_file = load(get_mark_filename('bond', day, tetrode_number));
 params = mark_data_file.filedata.params;
 ind = find(params(:, 1) / 1E4 >= linear_position_time(1) & ...
     params(:, 1) / 1E4 <= linear_position_time(end));
 spike_times = params(ind, 1);
-mark0 = params(ind, 2:5); % tetrode wire maxes
+marks = params(ind, 2:5); % tetrode wire maxes
 spike_time_in_seconds = spike_times / 1E4;
 [~, procInd1] = histc(spike_time_in_seconds, linear_position_time);
 [~, rawInd0] = histc(spike_time_in_seconds, spike_time_in_seconds);
 markAll(:, 1) = procInd1;
-markAll(:, 2:5) = mark0(rawInd0(rawInd0 ~= 0), :);
+markAll(:, 2:5) = marks(rawInd0(rawInd0 ~= 0), :);
 end
 
 function [filename] = get_mark_filename(animal, day, tetrode_number)
