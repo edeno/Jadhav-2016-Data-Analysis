1e38de97685a7a43e03d3dee4a7e7beb15cd7eb6
edeno <edeno@bu.edu>
2016-10-12 13:05:30 -0400

    Generalize code so an arbitrary number of marks can be used.

--- Xinyi-ripple-decoding/get_likelihood_by_tetrode.m
+++ Xinyi-ripple-decoding/get_likelihood_by_tetrode.m
@@ -1,12 +1,9 @@
 function [l2] = get_likelihood_by_tetrode(tet_sum, marks, ...
     mark_spikes_to_linear_position_time_bins_index_I, gaussian_kernel_position_estimator_I, ...
-    occ_Indicator, Lint_I, dt, smker)
-new_axis = ones(1, length(mark_spikes_to_linear_position_time_bins_index_I));
-l0 = normpdf(marks(tet_sum, 1) * new_axis, marks(mark_spikes_to_linear_position_time_bins_index_I, 1)', smker) .* ...
-    normpdf(marks(tet_sum, 2) * new_axis, marks(mark_spikes_to_linear_position_time_bins_index_I, 2)', smker) .* ...
-    normpdf(marks(tet_sum, 3) * new_axis, marks(mark_spikes_to_linear_position_time_bins_index_I, 3)', smker) .* ...
-    normpdf(marks(tet_sum, 4) * new_axis, marks(mark_spikes_to_linear_position_time_bins_index_I, 4)', smker);
-l1 = gaussian_kernel_position_estimator_I * l0' ./ occ_Indicator(:, 1) ./ dt;
-l2 = l1 .* dt .* exp(-Lint_I .* dt);
+    occ_Indicator, estimated_rate_by_tetrode, dt, smker)
+mark_spike_axis = ones(1, length(mark_spikes_to_linear_position_time_bins_index_I));
+l0 = prod(normpdf(marks(tet_sum(mark_spike_axis), :), marks(mark_spikes_to_linear_position_time_bins_index_I, :), smker), 2);
+l1 = gaussian_kernel_position_estimator_I * l0 ./ occ_Indicator(:, 1) ./ dt;
+l2 = l1 .* dt .* exp(-estimated_rate_by_tetrode .* dt);
 l2 = l2 ./ sum(l2);
 end
