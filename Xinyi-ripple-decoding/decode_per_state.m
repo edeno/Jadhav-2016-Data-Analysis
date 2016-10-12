cbca598054dfeb40a98a5cc9cfce3c79d395953d
edeno <edeno@bu.edu>
2016-10-12 06:36:45 -0400

    Rename L to likelihood

--- Xinyi-ripple-decoding/decode_per_state.m
+++ Xinyi-ripple-decoding/decode_per_state.m
@@ -1,4 +1,4 @@
-function L = decode_per_state(state_number, stateV_length, is_spike_at_time_t, tet_ind, tet_sum, markAll, procInd1_I, procInd1_Ia, Xnum_I, occ_I_Lambda, Lint_I, dt, smker)
+function likelihood = decode_per_state(state_number, stateV_length, is_spike_at_time_t, tet_ind, tet_sum, markAll, procInd1_I, procInd1_Ia, Xnum_I, occ_I_Lambda, Lint_I, dt, smker)
 l = zeros(stateV_length, length(is_spike_at_time_t));
 for j = 1:length(is_spike_at_time_t)
     tetrode_ind = find(tet_ind(is_spike_at_time_t(j), :));
@@ -7,6 +7,6 @@ for j = 1:length(is_spike_at_time_t)
         Xnum_I{tetrode_ind, state_number}, occ_I_Lambda{state_number}, ...
         Lint_I{tetrode_ind, state_number}, dt, smker);
 end
-L = prod(l, 2);
-L = L ./ sum(L);
+likelihood = prod(l, 2);
+likelihood = likelihood ./ sum(likelihood);
 end
