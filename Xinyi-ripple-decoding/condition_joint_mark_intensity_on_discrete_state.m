5ff0549d4656767ce58982f7064bac7b7826c5ce
edeno <edeno@bu.edu>
2016-10-12 11:27:45 -0400

    Fix missed renaming of procInd1

--- Xinyi-ripple-decoding/condition_joint_mark_intensity_on_discrete_state.m
+++ Xinyi-ripple-decoding/condition_joint_mark_intensity_on_discrete_state.m
@@ -1,9 +1,9 @@
-function [Lint_I, occ_I] = condition_joint_mark_intensity_on_discrete_state(xtrain, procInd1, is_state, sxker, mark_bins, linear_distance_bins, dt)
+function [Lint_I, occ_I] = condition_joint_mark_intensity_on_discrete_state(xtrain, mark_spikes_to_linear_position_time_bins_index, is_state, sxker, mark_bins, linear_distance_bins, dt)
 num_mark_bins = length(mark_bins);
 num_state = length(is_state);
 num_linear_distance_bins = length(linear_distance_bins);
 
-procInd1_I = procInd1(ismember(procInd1, is_state));
+mark_spikes_to_linear_position_time_bins_index_I = mark_spikes_to_linear_position_time_bins_index(ismember(mark_spikes_to_linear_position_time_bins_index, is_state));
 occ_I = normpdf(linear_distance_bins' * ones(1, num_state), ...
     ones(num_linear_distance_bins, 1) * xtrain(is_state), sxker) * ...
     ones(num_state, num_mark_bins);
--- Xinyi-ripple-decoding/encode_per_tetrode.m
+++ Xinyi-ripple-decoding/encode_per_tetrode.m
@@ -1,10 +1,11 @@
-function [procInd1_I, Xnum_I, Lint_I] = encode_per_tetrode( ...
-    procInd1, is_state,  occ_I, dt, linear_distance_bins, xtrain, sxker)
+function [mark_spikes_to_linear_position_time_bins_index_I, gaussian_kernel_position_estimator_I, Lint_I] = encode_per_tetrode( ...
+    mark_spikes_to_linear_position_time_bins_index, is_state,  occ_I, dt, linear_distance_bins, xtrain, sxker)
 
-procInd1_Ia = procInd1(ismember(procInd1, is_state));
-procInd1_I = find(ismember(procInd1, is_state));
 Xnum_I = normpdf(linear_distance_bins' * ones(1, length(procInd1_Ia)), ...
     ones(length(linear_distance_bins), 1) * xtrain(procInd1_Ia), ...
+position_time_bin_in_state = ismember(mark_spikes_to_linear_position_time_bins_index, is_state);
+mark_spikes_to_linear_position_time_bins_index_Ia = mark_spikes_to_linear_position_time_bins_index(position_time_bin_in_state);
+mark_spikes_to_linear_position_time_bins_index_I = find(position_time_bin_in_state);
     sxker);
 %Xnum: Gaussian kernel estimators for position
 Lint_I = sum(Xnum_I, 2) ./ occ_I(:,1) ./ dt; %integral
