e16f9d28c36d1417f313c22c154b29e95abea4fb
edeno <edeno@bu.edu>
2016-10-13 13:08:31 -0400

    Separating the loading of marks data from the kernel density model encoding

--- Xinyi-ripple-decoding/kernel_density_model.m
+++ Xinyi-ripple-decoding/kernel_density_model.m
@@ -1,13 +1,12 @@
-function [mark_spike_times, marks, mark_spikes_to_linear_position_time_bins_index] = kernel_density_model(animal, day, tetrode_number, ...
-    linear_position_time)
-mark_data_file = load(get_mark_filename('bond', day, tetrode_number));
-params = mark_data_file.filedata.params;
-mark_spike_times = params(:, 1);
+function [mark_spike_times, marks, mark_spikes_to_linear_position_time_bins_index] = kernel_density_model( ...
+    mark_spike_times, marks, linear_position_time)
+
 mark_spike_time_in_seconds = mark_spike_times / 1E4;
+
 ind = find(mark_spike_time_in_seconds >= linear_position_time(1) & ...
     mark_spike_time_in_seconds <= linear_position_time(end));
 
-marks = params(ind, 2:5); % tetrode wire maxes
+
 mark_spike_times = mark_spike_times(ind);
 mark_spike_time_in_seconds = mark_spike_times / 1E4;
 
