f99ff6e11afb2aa45abee7cb3a658b87ec29768c
edeno <edeno@bu.edu>
2016-10-13 10:47:15 -0400

    Move getting state transitions and initial conditions to separate functions

---
+++ Xinyi-ripple-decoding/get_initial_conditions.m
@@ -0,0 +1,15 @@
+function [initial_conditions] = get_initial_conditions(linear_distance_bins)
+num_linear_distance_bins = length(linear_distance_bins);
+linear_distance_bin_size = linear_distance_bins(2) - linear_distance_bins(1);
+%P(x0|I);
+Px_I{1} = exp(-linear_distance_bins.^2  ./ (2 * (2 * linear_distance_bin_size)^2));
+Px_I{1} = Px_I{1} ./ sum(Px_I{1});
+Px_I{2} = max(Px_I{1}) * ones(1, num_linear_distance_bins) - Px_I{1};
+Px_I{2} = Px_I{2} ./ sum(Px_I{2});
+
+%P(x0)=P(x0|I)P(I);
+initial_conditions(:, 1) = 0.25 * Px_I{1}'; % outbound forward
+initial_conditions(:, 2) = 0.25 * Px_I{2}'; % outbound reverse
+initial_conditions(:, 3) = 0.25 * Px_I{2}'; % inbound forward
+initial_conditions(:, 4) = 0.25 * Px_I{1}'; % inbound reverse
+end
---
+++ Xinyi-ripple-decoding/get_state_transition_model.m
@@ -0,0 +1,13 @@
+function [state_transition_model] = get_state_transition_model(state_index, linear_distance_bins, linear_distance)
+%% empirical movement transition matrix conditioned on discrete state
+num_discrete_states = length(state_index);
+empirical_movement_transition_matrix = cell(num_discrete_states, 1);
+for state_number = 1:num_discrete_states,
+    empirical_movement_transition_matrix{state_number} = condition_empirical_movement_transition_matrix_on_state(linear_distance_bins, linear_distance, state_index{state_number});
+end
+
+state_transition_model{1} = empirical_movement_transition_matrix{1}; % outbound forward
+state_transition_model{2} = empirical_movement_transition_matrix{2}; % outbound reverse
+state_transition_model{3} = empirical_movement_transition_matrix{2}; % inbound forward
+state_transition_model{4} = empirical_movement_transition_matrix{1}; % inbound reverse
+end
