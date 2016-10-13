03e6f16f3bded95c21ac93588c60d0a38017d93c
edeno <edeno@bu.edu>
2016-10-13 12:47:51 -0400

    Add comments

--- Xinyi-ripple-decoding/get_state_transition_model.m
+++ Xinyi-ripple-decoding/get_state_transition_model.m
@@ -8,7 +8,7 @@ for state_number = 1:num_discrete_states,
 end
 
 state_transition_model{1} = empirical_movement_transition_matrix{1}; % outbound forward
-state_transition_model{2} = empirical_movement_transition_matrix{2}; % outbound reverse
+state_transition_model{2} = empirical_movement_transition_matrix{2}; % outbound reverse, same as inbound forward
 state_transition_model{3} = empirical_movement_transition_matrix{2}; % inbound forward
-state_transition_model{4} = empirical_movement_transition_matrix{1}; % inbound reverse
+state_transition_model{4} = empirical_movement_transition_matrix{1}; % inbound reverse, same as outbound forward
 end
