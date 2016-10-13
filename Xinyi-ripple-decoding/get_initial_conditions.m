287b6b30db86e429afe87ee83717935986934a87
edeno <edeno@bu.edu>
2016-10-13 11:46:33 -0400

    Add comments describing why the initial conditions are set the way they are

--- Xinyi-ripple-decoding/get_initial_conditions.m
+++ Xinyi-ripple-decoding/get_initial_conditions.m
@@ -1,11 +1,16 @@
 function [initial_conditions] = get_initial_conditions(linear_distance_bins)
-%% Where the replay trajectory starts
+% Where the replay trajectory starts. Assume the replay event is equally 
+% likely to happen at any position in the maze
 num_linear_distance_bins = length(linear_distance_bins);
 linear_distance_bin_size = linear_distance_bins(2) - linear_distance_bins(1);
-%P(x0|I);
-Px_I{1} = exp(-linear_distance_bins.^2  ./ (2 * (2 * linear_distance_bin_size)^2));
+%% P(x0|I)
+% Gaussian with probability mass at center arm reflecting that the replay
+% trajectory is likely to start at the center arm
 Px_I{1} = normpdf(linear_distance_bins, 0, linear_distance_bin_size * 2);
 Px_I{1} = Px_I{1} ./ sum(Px_I{1});
+% Gaussian with probability mass everywhere but at the center arm
+% reflecting that the replay trajectory is likely to start everywhere
+% except at the center arm
 Px_I{2} = max(Px_I{1}) * ones(1, num_linear_distance_bins) - Px_I{1};
 Px_I{2} = Px_I{2} ./ sum(Px_I{2});
 
@@ -14,4 +19,5 @@ initial_conditions(:, 1) = 0.25 * Px_I{1}'; % outbound forward
 initial_conditions(:, 2) = 0.25 * Px_I{2}'; % outbound reverse
 initial_conditions(:, 3) = 0.25 * Px_I{2}'; % inbound forward
 initial_conditions(:, 4) = 0.25 * Px_I{1}'; % inbound reverse
+%% P(x0)=P(x0|I)P(I);
 end
