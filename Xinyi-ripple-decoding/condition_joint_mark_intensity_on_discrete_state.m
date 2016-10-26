5a976f709b5ba7a07620683d6636d952a1262026
edeno <edeno@bu.edu>
2016-10-25 22:01:06 -0400

    Make consistent

--- Xinyi-ripple-decoding/condition_joint_mark_intensity_on_discrete_state.m
+++ Xinyi-ripple-decoding/condition_joint_mark_intensity_on_discrete_state.m
@@ -9,9 +9,9 @@ num_spikes_axis = ones(1, length(mark_spikes_to_linear_position_time_bins_index)
 
 position_occupancy = sum(normpdf(linear_distance_bins(state_axis, :)', linear_distance(linear_distance_bins_axis, state_index), linear_distance_bin_size), 2);
 linear_position_at_spike = linear_distance(linear_distance_bins_axis, mark_spikes_to_linear_position_time_bins_index);
-gaussian_kernel_position_estimator = sum(normpdf(linear_distance_bins(num_spikes_axis, :)', ...
-     linear_position_at_spike, linear_distance_bin_size), 2);
+gaussian_kernel_position_estimator = normpdf(linear_distance_bins(num_spikes_axis, :)', ...
+     linear_position_at_spike, linear_distance_bin_size);
 
-estimated_rate = gaussian_kernel_position_estimator ./ position_occupancy ./ dt; %integral
+estimated_rate = sum(gaussian_kernel_position_estimator, 2) ./ position_occupancy ./ dt; %integral
 estimated_rate = normalize_distribution(estimated_rate);
 end
