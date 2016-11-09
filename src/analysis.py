7da749e1b8bcc6b7a00f9d328e52a1e07482f448
edeno <edeno@bu.edu>
2016-11-09 11:36:22 -0500

    Save to hdf5 instead of returning dictionary

--- src/analysis.py
+++ src/analysis.py
@@ -19,32 +19,78 @@ def coherence_by_ripple_type(epoch_index, animals, ripple_covariate,
 
     grouped = ripple_info.groupby(ripple_covariate)
     window_of_interest = multitaper_params.pop('window_of_interest')
-    coherence_by_ripple_type = {ripple_type: _get_coherence_for_all_pairs(lfps, ripples,
-                                                                          multitaper_params,
-                                                                          window_of_interest)
-                                for ripple_type, ripples in grouped}
 
-    difference_of_levels = itertools.combinations(grouped.groups.keys(), 2)
-    for level1, level2 in difference_of_levels:
-        group_name = '{level2} - {level1}'.format(level1=level1, level2=level2)
-        coherence_by_ripple_type[group_name] = {
-            tetrode_pair: spectral.power_and_coherence_change(
-                coherence_by_ripple_type[level1][tetrode_pair],
-                coherence_by_ripple_type[level2][tetrode_pair])
-            for tetrode_pair in coherence_by_ripple_type[level1]}
+    for level_name, ripples_df in grouped:
+        ripple_times_by_group = _get_ripple_times(ripples_df)
+        reshaped_lfps = {key: data_processing.reshape_to_segments(
+            lfps[key], ripple_times_by_group, window_offset=window_of_interest, concat_axis=1)
+                         for key in lfps}
+        for tetrode1, tetrode2 in itertools.combinations(sorted(reshaped_lfps), 2):
+            coherence_df = spectral.multitaper_coherogram(
+                [reshaped_lfps[tetrode1], reshaped_lfps[tetrode2]], **multitaper_params)
+            save_tetrode_pair(coherence_name, ripple_covariate, level_name,
+                              tetrode1, tetrode2, coherence_df)
 
-    return coherence_by_ripple_type
+    for level1, level2 in itertools.combinations(grouped.groups.keys(), 2):
+        level_difference_name = '{level2}_{level1}'.format(level1=level1, level2=level2)
+        for tetrode1, tetrode2 in itertools.combinations(sorted(reshaped_lfps), 2):
+            level1_coherence_df = get_tetrode_pair_from_hdf(
+                coherence_name, ripple_covariate, level1, tetrode1, tetrode2)
+            level2_coherence_df = get_tetrode_pair_from_hdf(
+                coherence_name, ripple_covariate, level2, tetrode1, tetrode2)
+            coherence_difference_df = spectral.power_and_coherence_change(
+                level1_coherence_df, level2_coherence_df)
+            save_tetrode_pair(coherence_name, ripple_covariate, level_difference_name,
+                              tetrode1, tetrode2, coherence_difference_df)
 
-
-def _get_coherence_for_all_pairs(lfps, ripples_df, multitaper_params, window_of_interest):
-    ripple_times = _get_ripple_times(ripples_df)
-    reshaped_lfps = {key: data_processing.reshape_to_segments(
-        lfps[key], ripple_times, window_offset=window_of_interest, concat_axis=1)
-                     for key in lfps}
-    return {(lfp1, lfp2): spectral.multitaper_coherogram(
-        [reshaped_lfps[lfp1], reshaped_lfps[lfp2]], **multitaper_params)
-            for lfp1, lfp2 in itertools.combinations(reshaped_lfps.keys(), 2)}
+    multitaper_params['window_of_interest'] = window_of_interest
+    save_multitaper_parameters(epoch_index, coherence_name, multitaper_params)
+    save_ripple_info(epoch_index, ripple_info)
+    save_tetrode_pair_info(epoch_index, coherence_name, tetrode_info)
 
 
 def _get_ripple_times(df):
     return df.loc[:, ('ripple_start_time', 'ripple_end_time')].values.tolist()
+
+
+def save_tetrode_pair(coherence_name, covariate, level, tetrode1, tetrode2, save_df):
+    animal, day, epoch = tetrode1[0:3]
+    hdf_path = tetrode_pair_hdf_path(coherence_name, covariate, level, tetrode1[-1], tetrode2[-1])
+    with pd.HDFStore(analysis_file_path(animal, day, epoch)) as store:
+        store.put(hdf_path, save_df)
+
+
+def get_tetrode_pair_from_hdf(coherence_name, covariate, level, tetrode1, tetrode2):
+    animal, day, epoch = tetrode1[0:3]
+    hdf_path = tetrode_pair_hdf_path(coherence_name, covariate, level, tetrode1[-1], tetrode2[-1])
+    return pd.read_hdf(analysis_file_path(animal, day, epoch), key=hdf_path)
+
+
+def tetrode_pair_hdf_path(coherence_name, covariate, level, tetrode1, tetrode2):
+    return '{coherence_name}/tetrode{tetrode1}_tetrode{tetrode2}/{covariate}/{level}'.format(
+        coherence_name=coherence_name, covariate=covariate,
+        level=level, tetrode1=tetrode1, tetrode2=tetrode2)
+
+
+def analysis_file_path(animal, day, epoch):
+    filename = '{animal}_{day}_{epoch}.h5'.format(animal=animal, day=day, epoch=epoch)
+    return os.path.join(os.path.abspath(os.path.pardir), 'Processed-Data', filename)
+
+
+def save_multitaper_parameters(epoch_index, coherence_name, multitaper_params):
+    coherence_node_name = '/{coherence_name}'.format(coherence_name=coherence_name)
+    with pd.HDFStore(analysis_file_path(*epoch_index)) as store:
+        store.get_node(coherence_node_name)._v_attrs.multitaper_parameters = multitaper_params
+
+
+def save_ripple_info(epoch_index, ripple_info):
+    with pd.HDFStore(analysis_file_path(*epoch_index)) as store:
+        store.put('/ripple_info', ripple_info)
+
+
+def save_tetrode_pair_info(epoch_index, coherence_name, tetrode_info):
+    hdf_path = '{coherence_name}/tetrode_pair_info'.format(coherence_name=coherence_name)
+    with pd.HDFStore(analysis_file_path(*epoch_index)) as store:
+        store.put(hdf_path, data_processing.get_tetrode_pair_info(tetrode_info))
+
+
