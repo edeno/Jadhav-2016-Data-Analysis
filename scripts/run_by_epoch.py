a6366bd6ac8599e6030e14ca4510a85a1230bce5
edeno <edeno@bu.edu>
2016-11-14 11:31:04 -0500

    Initial script for running code on the cluster for each epoch

---
+++ scripts/run_by_epoch.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import collections
+sys.path.append('../src/')
+import ripple_detection
+
+sampling_frequency = 1500
+Animal = collections.namedtuple('Animal', {'directory', 'short_name'})
+animals = {
+    'HPa': Animal(directory='HPa_direct', short_name='HPa'),
+    'HPc': Animal(directory='HPc_direct', short_name='HPc')
+}
+gamma_frequency_params = dict(
+    sampling_frequency=sampling_frequency,
+    time_window_duration=0.600,
+    time_window_step=0.600,
+    desired_frequencies=[20, 100],
+    time_halfbandwidth_product=3,
+    window_of_interest=(-2.100, 2.100)
+)
+low_frequency_params = dict(
+    sampling_frequency=1500,
+    time_window_duration=3.000,
+    time_window_step=3.000,
+    desired_frequencies=[3, 20],
+    time_halfbandwidth_product=3,
+    window_of_interest=(-7.500, 7.500)
+)
+coherence_type = {
+    'gamma_coherence': gamma_frequency_params,
+    'low_frequency_coherence': low_frequency_params
+}
+ripple_covariates = ['is_spike', 'session_time', 'ripple_trajectory', 'ripple_direction']
+
+
+def coherence_by_ripple_type(epoch_index):
+    ripple_times = ripple_detection.get_epoch_ripples(
+        epoch_index, animals, sampling_frequency=sampling_frequency)
+    print(ripple_times)
+
+
+def main():
+    epoch_index = (sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))  # animal, day, epoch
+    coherence_by_ripple_type(epoch_index)
+
+if __name__ == '__main__':
+    sys.exit(main())
