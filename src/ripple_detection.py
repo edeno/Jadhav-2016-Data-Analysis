59d7d244d9bfc7a8aae29a0f899b8c184184e31f
edeno <edeno@bu.edu>
2016-12-21 13:49:56 -0500

    Add some documentation

--- src/ripple_detection.py
+++ src/ripple_detection.py
@@ -13,7 +13,7 @@ import data_processing
 
 
 def _get_computed_ripple_times(tetrode_tuple, animals):
-    ''' Returns a list of tuples for a given tetrode in the format
+    '''Returns a list of tuples for a given tetrode in the format
     (start_index, end_index). The indexes are relative
     to the trial time for that session. Data is extracted from the ripples
     data structure and calculated according to the Frank Lab criterion.
@@ -26,7 +26,7 @@ def _get_computed_ripple_times(tetrode_tuple, animals):
 
 
 def get_computed_consensus_ripple_times(epoch_index, animals):
-    ''' Returns a list of tuples for a given epoch in the format
+    '''Returns a list of tuples for a given epoch in the format
     (start_time, end_time).
     '''
     animal, day, epoch_ind = epoch_index
@@ -36,7 +36,7 @@ def get_computed_consensus_ripple_times(epoch_index, animals):
 
 
 def get_computed_ripples_dataframe(tetrode_index, animals):
-    ''' Given a tetrode index (animal, day, epoch, tetrode #), returns a pandas dataframe
+    '''Given a tetrode index (animal, day, epoch, tetrode #), returns a pandas dataframe
     with the pre-computed ripples from the Frank lab labeled according to the ripple number.
     Non-ripple times are marked as NaN.
     '''
@@ -49,7 +49,7 @@ def get_computed_ripples_dataframe(tetrode_index, animals):
 
 
 def _convert_ripple_times_to_dataframe(ripple_times, dataframe):
-    ''' Given a list of ripple time tuples (ripple #, start time, end time) and a dataframe with a
+    '''Given a list of ripple time tuples (ripple #, start time, end time) and a dataframe with a
     time index (such as the lfp dataframe), returns a pandas dataframe with a column with the
     timestamps of each ripple labeled according to the ripple number. Non-ripple times are marked
     as NaN.
@@ -72,7 +72,7 @@ def _convert_ripple_times_to_dataframe(ripple_times, dataframe):
 
 
 def _get_series_start_end_times(series):
-    ''' Returns a two element tuple with of the start of the segment and the end of the segment.
+    '''Returns a two element tuple with of the start of the segment and the end of the segment.
     Each element is an numpy array, The input series must be a boolean pandas series where the
     index is time.
     '''
@@ -86,10 +86,9 @@ def _get_series_start_end_times(series):
 
 
 def segment_boolean_series(series, minimum_duration=0.015):
-    ''' Returns a list of tuples where each tuple contains the
+    '''Returns a list of tuples where each tuple contains the
     start time of segement and end time of segment. It takes
     a boolean pandas series as input where the index is time.
-
     '''
     start_times, end_times = _get_series_start_end_times(series)
 
@@ -136,7 +135,7 @@ def Karlsson_method(lfps, smoothing_sigma=0.004, sampling_frequency=1500,
 def get_epoch_ripples(epoch_index, animals, sampling_frequency,
                       ripple_detection_function=Kay_method,
                       ripple_detection_kwargs={}, speed_threshold=4):
-    ''' Returns a list of tuples containing the start and end times of ripples. Candidate ripples
+    '''Returns a list of tuples containing the start and end times of ripples. Candidate ripples
     are computed via the ripple detection function and then filtered to exclude ripples where the
     animal was still moving.
     '''
@@ -154,7 +153,7 @@ def get_epoch_ripples(epoch_index, animals, sampling_frequency,
 
 
 def _exclude_movement_during_ripples(ripple_times, epoch_index, animals, speed_threshold):
-    ''' Excludes ripples where the head direction speed is greater than the speed threshold.
+    '''Excludes ripples where the head direction speed is greater than the speed threshold.
     Only looks at the start of the ripple to determine head movement speed for the ripple.
     '''
     position_df = data_processing.get_interpolated_position_dataframe(epoch_index, animals)
@@ -163,7 +162,7 @@ def _exclude_movement_during_ripples(ripple_times, epoch_index, animals, speed_t
 
 
 def _get_smoothed_envelope(lfp, sigma, sampling_frequency):
-    ''' Filters the lfp between 150-250 Hz and returns the
+    '''Filters the lfp between 150-250 Hz and returns the
     smoothed envelope of the filtered signal
     '''
     return pd.Series(_smooth(_get_envelope(_ripple_bandpass_filter(lfp.values.flatten())),
@@ -172,6 +171,36 @@ def _get_smoothed_envelope(lfp, sigma, sampling_frequency):
 
 def _get_candidate_ripples_Kay(filtered_lfps, is_multitaper=False, minimum_duration=0.015,
                                zscore_threshold=2, sigma=0.004, sampling_frequency=1500):
+    '''Candidate ripple times are extracted based on mean power on all tetrodes
+
+    Using power from all tetrodes reflects that we view the ripple as a population event
+
+    Parameters
+    ----------
+    filtered_lfps : Pandas series or array_like
+        Time series of ripple band power
+    is_multitaper : bool
+        Are we using multi-taper methods to extract the power?
+    minimum_duration : float, optional
+        Minimum time the z-score has to stay above threshold to be considered a
+        candidate ripple
+    zscore_threshold : float, optional
+        Number of standard deviations the ripple power must exceed to be considered
+        a candidate ripple
+    sigma : float, optional
+    sampling_frequency : int, optional
+
+    Returns
+    -------
+    ripple_times : list of 2-element tuples
+        The elements correspond to the start and stop times of the ripple
+
+    References
+    ----------
+    .. [1] Kay, K., Sosa, M., Chung, J.E., Karlsson, M.P., Larkin, M.C., and Frank, L.M. (2016).
+    A hippocampal network for spatial coding during immobility and sleep.
+    Nature 531, 185–190.
+    '''
     combined_lfps = np.sum(pd.concat(filtered_lfps, axis=1) ** 2, axis=1)
 
     # Don't need to smooth if using multitaper method
@@ -191,6 +220,32 @@ def _get_candidate_ripples_Kay(filtered_lfps, is_multitaper=False, minimum_durat
 
 def _get_candidate_ripples_Karlsson(filtered_lfps, minimum_duration=0.015,
                                     zscore_threshold=2):
+    '''Candidate ripple times are detected on each tetrode and then combined
+    if they overlap
+
+    Parameters
+    ----------
+    filtered_lfps : Pandas series or array_like
+        Time series of ripple band power
+    minimum_duration : float, optional
+        Minimum time the z-score has to stay above threshold to be considered a
+        candidate ripple
+    zscore_threshold : float, optional
+        Number of standard deviations the ripple power must exceed to be considered
+        a candidate ripple
+
+    Returns
+    -------
+    ripple_times : list of 2-element tuples
+        The elements correspond to the start and stop times of the ripple
+
+
+    References
+    ----------
+    .. [1] Karlsson, M.P., and Frank, L.M. (2009).
+    Awake replay of remote experiences in the hippocampus.
+    Nature Neuroscience 12, 913–918.
+    '''
     thresholded_lfps = [_threshold_by_zscore(lfp, zscore_threshold=zscore_threshold)
                         for lfp in filtered_lfps]
     extended_lfps = [_extend_threshold_to_mean(threshold_df.is_above_mean,
@@ -201,12 +256,27 @@ def _get_candidate_ripples_Karlsson(filtered_lfps, minimum_duration=0.015,
 
 
 def _flatten_list(original_list):
+    '''Takes a list of lists and turns it into a single list'''
     return [item for sublist in original_list for item in sublist]
 
 
-def _get_ripple_power_multitaper(lfp, sampling_frequency, time_halfbandwidth_product=1,
+def _get_ripple_power_multitaper(lfp, sampling_frequency,
+                                 time_halfbandwidth_product=1,
                                  time_window_duration=0.020,
                                  time_window_step=0.004):
+    '''Extracts the smoothed spectral power time course
+    in the ripple frequency band (150-250 Hz)
+
+    Parameters
+    ----------
+    lfp : Pandas series or array_like
+        Local field electric potential with time as the index
+    sampling_frequency : int
+
+    Returns
+    -------
+    ripple_power : Pandas series
+    '''
     return spectral.multitaper_spectrogram(
         lfp,
         time_halfbandwidth_product=time_halfbandwidth_product,
@@ -218,14 +288,14 @@ def _get_ripple_power_multitaper(lfp, sampling_frequency, time_halfbandwidth_pro
 
 
 def _ripple_bandpass_filter(data):
-    ''' Returns a bandpass filtered signal between 150-250 Hz using the Frank lab filter
+    '''Returns a bandpass filtered signal between 150-250 Hz using the Frank lab filter
     '''
     filter_numerator, filter_denominator = _get_ripplefilter_kernel()
     return scipy.signal.filtfilt(filter_numerator, filter_denominator, data, axis=0)
 
 
 def _get_ripplefilter_kernel():
-    ''' Returns the pre-computed ripple filter kernel from the Frank lab. The kernel is 150-250 Hz
+    '''Returns the pre-computed ripple filter kernel from the Frank lab. The kernel is 150-250 Hz
     bandpass with 40 db roll off and 10 Hz sidebands.
     '''
     data_dir = '{working_dir}/Raw-Data'.format(
@@ -236,61 +306,135 @@ def _get_ripplefilter_kernel():
 
 
 def _extend_threshold_to_mean(is_above_mean, is_above_threshold, minimum_duration=0.015):
+    '''Extract segments above threshold if they remain above the threshold
+    for a minimum amount of time and extend them to the mean
+
+    Parameters
+    ----------
+    is_above_mean : Pandas series
+        Time series indicator function specifying when the
+        time series is above the mean. Index of the series is time.
+    is_above_threshold : Pandas series
+        Time series indicator function specifying when the
+        time series is above the the threshold. Index of the series is time.
+
+    Returns
+    -------
+    extended_segments : list of 2-element tuples
+        Elements correspond to the start and end time of segments
+    '''
     above_mean_segments = segment_boolean_series(is_above_mean,
                                                  minimum_duration=minimum_duration)
     above_threshold_segments = segment_boolean_series(is_above_threshold,
                                                       minimum_duration=minimum_duration)
-    return _extend_segment_intervals(above_threshold_segments, above_mean_segments)
+    return _extend_segment(above_threshold_segments, above_mean_segments)
 
 
 def _find_containing_interval(interval_candidates, target_interval):
     '''Returns the interval that contains the target interval out of a list of
-    interval candidates. This is accomplished by finding the closest start time
+    interval candidates.
+
+    This is accomplished by finding the closest start time
     out of the candidate intervals, since we already know that one interval candidate
     contains the target interval (the segements above 0 contain the segments above
-    the threshold)'''
+    the threshold)
+    '''
     candidate_start_times = np.asarray(interval_candidates)[:, 0]
     closest_start_ind = np.max(
         (candidate_start_times - target_interval[0] <= 0).nonzero())
     return interval_candidates[closest_start_ind]
 
 
-def _extend_segment_intervals(ripple_above_threshold_segments, ripple_above_mean_segments):
-    ''' Returns a list of tuples that extend the
-    boundaries of the segments by the ripple threshold (i.e ripple z-score > 3)
-    to the boundaries of a containing interval defined by when the z-score
-    crosses the mean.
+def _extend_segment(segments_to_extend, containing_segments):
+    '''Extends the boundaries of a segment if it is
+    a subset of one of the containing segments
+
+    Parameters
+    ----------
+    segments_to_extend : list of 2-element tuples
+        Elements are the start and end times
+    containing_segments : list of 2-element tuples
+        Elements are the start and end times
+
+    Returns
+    -------
+    extended_segments : list of 2-element tuples
     '''
-    segments = [_find_containing_interval(ripple_above_mean_segments, segment)
-                for segment in ripple_above_threshold_segments]
+    segments = [_find_containing_interval(containing_segments, segment)
+                for segment in segments_to_extend]
     return list(set(segments))  # remove duplicate segments
 
 
 def _get_envelope(data, axis=0):
+    '''Extracts the instantaneous amplitude (envelope) of an analytic signal
+    using the Hilbert transform'''
     return np.abs(scipy.signal.hilbert(data, axis=axis))
 
 
-def _smooth(data, sigma, sampling_frequency):
+def _smooth(data, sigma, sampling_frequency, axis=0, truncate=8):
+    '''1D convolution of the data with a Gaussian.
+
+    The standard deviation of the gaussian is in the units of the sampling frequency.
+    The function is just a wrapper around scipy's gaussian_filter1d,
+    The support is truncated at 8 by default, instead of 4 in gaussian_filter1d
+
+    Parameters
+    ----------
+    data : array_like
+    sigma : float
+    sampling_frequency : int
+    axis : int, optional
+    truncate : int, optional
+
+    Returns
+    -------
+    smoothed_data : array_like
+    '''
     return scipy.ndimage.filters.gaussian_filter1d(
-        data, sigma * sampling_frequency, truncate=8, axis=0)
+        data, sigma * sampling_frequency, truncate=truncate, axis=axis)
 
 
 def _threshold_by_zscore(data, zscore_threshold=2):
+    '''Standardize the data and determine whether it is above a given number
+
+    Parameters
+    ----------
+    data : Pandas series
+    zscore_threshold : int, optional
+
+    Returns
+    -------
+    threshold_dataframe : Pandas dataframe
+        Dataframe contains two columns.
+        One column is an indicator function where 1 indicates the z-score
+        is above the mean (z-score = 0) and 0 indicates the z-score is below the mean
+        The other column is an indicator function of zscored data
+        where 1 indicates the z-score is above the threshold parameter and
+        0 indicates the z-score is below the threshold parameter.
+    '''
     zscored_data = _zscore(data).values.flatten()
     return pd.DataFrame({'is_above_threshold': zscored_data >= zscore_threshold,
                          'is_above_mean': zscored_data >= 0}, index=data.index)
 
 
 def _zscore(x):
-    ''' Returns an array of the z-score of x
-    '''
+    '''The standardized score of x'''
     return (x - x.mean()) / x.std()
 
 
 def _merge_ranges(ranges):
-    """
-    Merge overlapping and adjacent ranges and yield the merged ranges
-    in order. The argument must be an iterable of pairs (start, stop).
+    """Merge overlapping and adjacent ranges
+
+    Parameters
+    ----------
+    ranges : iterable with 2-elements
+        Element 1 is the start of the range.
+        Element 2 is the end of the range.
+    Yields
+    -------
+    sorted_merged_range : 2-element tuple
+        Element 1 is the start of the merged range.
+        Element 2 is the end of the merged range.
 
     >>> list(merge_ranges([(5,7), (3,5), (-1,3)]))
     [(-1, 7)]
@@ -298,8 +442,9 @@ def _merge_ranges(ranges):
     [(1, 2), (3, 4), (5, 6)]
     >>> list(merge_ranges([]))
     []
-    from:
-        http://codereview.stackexchange.com/questions/21307/consolidate-list-of-ranges-that-overlap
+    References
+    ----------
+    .. [1] http://codereview.stackexchange.com/questions/21307/consolidate-list-of-ranges-that-overlap
     """
     ranges = iter(sorted(ranges))
     current_start, current_stop = next(ranges)
