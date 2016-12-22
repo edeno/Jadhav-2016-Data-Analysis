15fa7db186450ab9f7771fc50b13c61a31942efd
edeno <edeno@bu.edu>
2016-12-22 09:34:38 -0500

    Specify the amount of memory per core

--- scripts/queue_cluster_jobs.py
+++ scripts/queue_cluster_jobs.py
@@ -13,7 +13,7 @@ def main():
     os.makedirs(log_directory,  exist_ok=True)
 
     python_function = 'run_by_epoch.py'
-    directives = '-l h_rt=10:00:00 -pe omp 8 -P braincom'
+    directives = '-l h_rt=10:00:00 -pe omp 8 -P braincom -l mem_per_core=3G'
 
     Animal = collections.namedtuple('Animal', {'directory', 'short_name'})
     num_days = 8
