06aac67322dc604bcebcace20e2946061d2ade4b
edeno <edeno@bu.edu>
2017-01-19 16:24:49 -0500

    Add more tests for ripple decoding

--- tests/test_ripple_decoding.py
+++ tests/test_ripple_decoding.py
@@ -1,38 +1,146 @@
+from unittest.mock import patch
+
 import numpy as np
-import pytest
-from scipy.stats import multivariate_normal
+from pytest import mark
+from scipy.stats import multivariate_normal, norm
+from scipy.linalg import block_diag
 
-from src.ripple_decoding import _mark_space_estimator
+from src.ripple_decoding import (_fix_zero_bins, evaluate_mark_space,
+                                 _normalize_column_probability,
+                                 combined_likelihood,
+                                 normalize_to_probability,
+                                 estimate_place_field,
+                                 estimate_ground_process_intensity,
+                                 estimate_place_occupancy)
 
 
-def test__mark_estimator():
-    n_signals, n_marks, n_training_spikes, mark_smoothing = 20, 4, 10, 1
+def test_evaluate_mark_space():
+    n_marks, n_training_spikes, mark_std_deviation = 4, 10, 1
 
-    test_marks = np.zeros((n_signals, n_marks)) * np.nan
-    cur_spiking_neuron_ind = [0, 10]
-    test_marks[cur_spiking_neuron_ind[0], :] = np.arange(1, 9, 2)
-    test_marks[cur_spiking_neuron_ind[1], :] = np.arange(9, 17, 2)
+    test_marks = np.arange(1, 9, 2)
 
-    training_marks = np.zeros((n_training_spikes, n_signals, n_marks))
-    training_marks[3, cur_spiking_neuron_ind, :] = np.arange(1, 9, 2)
+    training_marks = np.zeros((n_training_spikes, n_marks))
+    training_marks[3, :] = np.arange(9, 17, 2)
 
-    mark_space_estimator = _mark_space_estimator(
+    mark_space_estimator = evaluate_mark_space(
         test_marks, training_marks=training_marks,
-        mark_smoothing=mark_smoothing)
+        mark_std_deviation=mark_std_deviation)
 
-    true_mark1 = multivariate_normal(
-        mean=np.arange(1, 9, 2),
-        cov=np.identity(n_marks) * mark_smoothing).pdf(np.arange(1, 9, 2))
-    true_mark2 = multivariate_normal(
+    expected_mark1 = multivariate_normal(
         mean=np.arange(9, 17, 2),
-        cov=np.identity(n_marks) * mark_smoothing).pdf(np.arange(1, 9, 2))
-    true_mark3 = multivariate_normal(
+        cov=np.identity(n_marks) * mark_std_deviation).pdf(
+            np.arange(1, 9, 2))
+    expected_mark2 = multivariate_normal(
         mean=np.zeros(n_marks,),
-        cov=np.identity(n_marks) * mark_smoothing).pdf(np.arange(1, 9, 2))
+        cov=np.identity(n_marks) * mark_std_deviation).pdf(
+            np.arange(1, 9, 2))
+    expected = np.ones((n_training_spikes,)) * expected_mark2
+    expected[3] = expected_mark1
 
+    assert np.allclose(mark_space_estimator, expected)
+
+
+def test__normalize_column_probability():
+    '''All columns should sum to one after normalization
+    '''
+    transition_matrix = np.arange(1, 10).reshape(3, 3)
+    expected = np.ones((3,))
     assert np.allclose(
-        mark_space_estimator[cur_spiking_neuron_ind[0], 3], true_mark1)
+        _normalize_column_probability(transition_matrix).sum(axis=0),
+        expected)
+
+
+def test__fix_zero_bins():
+    '''A column of all zeros should be set to ones
+    '''
+    transition_matrix = np.arange(0, 9).reshape(3, 3)
+    transition_matrix[:, 2] = 0
+    expected = np.copy(transition_matrix)
+    expected[:, 2] = 1
+    assert np.allclose(_fix_zero_bins(transition_matrix), expected)
+
+
+def test_normalize_to_probability():
+    ''' A vector should normalize to one
+    '''
+    x = np.arange(1, 9)
+    assert normalize_to_probability(x).sum() == 1
+
+
+@mark.parametrize('data, exponent, expected', [
+    (np.arange(1, 9), 1, np.nanprod(np.arange(1, 9))),
+    (np.arange(1, 9), 2, np.nanprod(np.arange(1, 9) ** 2)),  # test kwarg
+    (np.arange(1, 9).reshape(2, 4), 2,  # test product along 1st dimension
+     np.nanprod(np.arange(1, 9).reshape(2, 4) ** 2, axis=0)),
+    (2, 2, 4),  # test single data point
+])
+def test_combined_likelihood(data, exponent, expected):
+    def likelihood_function(x, exponent=1):
+        return x ** exponent
     assert np.allclose(
-        mark_space_estimator[cur_spiking_neuron_ind[1], 3], true_mark2)
-    assert np.allclose(
-        mark_space_estimator[cur_spiking_neuron_ind[0], 2], true_mark3)
+        combined_likelihood(data, likelihood_function,
+                            likelihood_kwargs=dict(exponent=exponent)),
+        expected)
+
+
+# @patch('_mark_space_estimator')
+# def test_joint_mark_intensity(mock_mark_space_estimator):
+#     n_signals, n_training_spikes, n_parameters, n_states = 2, 10, 6, 2
+#     mock_mark_space_estimator.return_value = np.ones(
+#         (n_signals, n_training_spikes))
+#     place_field_estimator = np.zeros(
+#         (n_signals, n_training_spikes, n_parameters))
+#     place_field_estimator[0, :, :] = block_diag(
+#         np.ones((n_training_spikes, int(n_parameters / n_states))),
+#         np.ones((n_training_spikes, int(n_parameters / n_states))) * 2)
+#     place_field_estimator[1, :, :] = block_diag(
+#         np.ones((n_training_spikes, int(n_parameters / n_states))) * 3,
+#         np.ones((n_training_spikes, int(n_parameters / n_states))) * 4)
+#     pass
+
+
+def test_get_ground_process_intensity():
+    place_field_estimator = np.zeros((61, 2))
+    place_field_estimator[30, 0] = 1
+    place_field_estimator[31, 1] = 1
+
+    place_occupancy = np.ones((61,))
+    place_occupancy[31] = 4
+    ground_process_intensity = estimate_ground_process_intensity(
+        place_field_estimator, place_occupancy)
+
+    expected = np.zeros((61,))
+    expected[30] = 0.8
+    expected[31] = 0.2
+
+    assert np.allclose(ground_process_intensity, expected)
+
+
+def test_estimate_place_field():
+    place_bins = np.linspace(0, 150, 61)
+    place_at_spike = np.asarray([25, 100])
+    place_std_deviation = 20
+    place_field_estimator = estimate_place_field(
+        place_bins, place_at_spike,
+        place_std_deviation=place_std_deviation)
+
+    expected1 = norm.pdf(
+        place_bins, place_at_spike[0], place_std_deviation)
+    expected2 = norm.pdf(
+        place_bins, place_at_spike[1], place_std_deviation)
+
+    assert np.allclose(place_field_estimator,
+                       np.vstack((expected1, expected2)).T)
+
+
+def test_estimate_place_occupancy():
+    place_bins = np.linspace(0, 150, 61)
+    place = np.asarray([25, 100])
+    place_std_deviation = 20
+    place_occupancy = estimate_place_occupancy(
+        place_bins, place, place_std_deviation=place_std_deviation)
+    expected1 = norm.pdf(
+        place_bins, place[0], place_std_deviation)
+    expected2 = norm.pdf(
+        place_bins, place[1], place_std_deviation)
+    assert np.allclose(place_occupancy, expected1 + expected2)
