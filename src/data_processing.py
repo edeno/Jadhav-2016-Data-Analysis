fa3e6f7bb2c45ab6840d0e231363782ce7a123ac
edeno <edeno@bu.edu>
2017-02-02 11:22:27 -0500

    Add functions to access marks data

--- src/data_processing.py
+++ src/data_processing.py
@@ -540,5 +540,60 @@ def get_area_pair_info(tetrode_info, epoch_index):
             .set_index(
                 ['animal', 'day', 'epoch', 'area1', 'area2'], drop=False))
 
+
+def get_mark_dataframe(tetrode_index, animals):
+    '''Retrieve the marks for each tetrode given a tetrode index
+
+    Parameters
+    ----------
+    tetrode_index : tuple
+        Elements are (animal_short_name, day, epoch, tetrode_number)
+    animals : dict of named-tuples
+        Dictionary containing information about the directory for each
+        animal. The key is the animal_short_name.
+
+    Returns
+    -------
+    mark_dataframe : pandas dataframe
+        The dataframe index is the time at which the mark occurred
+        (in seconds). THe other values are values that can be used as the
+        marks.
+    '''
+    TIME_CONSTANT = 1E4
+    mark_file = loadmat(get_mark_filename(tetrode_index, animals))
+    mark_names = [name[0][0].lower().replace(' ', '_')
+                  for name in mark_file['filedata'][0, 0]['paramnames']]
+    mark_data = mark_file['filedata'][0, 0]['params']
+    mark_data[:, mark_names.index('time')] = mark_data[
+        :, mark_names.index('time')] / TIME_CONSTANT
+
+    return pd.DataFrame(mark_data, columns=mark_names).set_index('time')
+
+
+def get_mark_filename(tetrode_index, animals):
+    '''Given an index tuple (animal, day, epoch, tetrode_number) and the
+    animals dictionary return a file name for the tetrode file marks
+    '''
+    data_dir = join(abspath(pardir), 'Raw-Data')
+    animal, day, _, tetrode_number = tetrode_index
+    filename = '{animal.short_name}marks{day:02d}-{tetrode_number:02d}.mat'.format(
+        data_dir=data_dir,
+        animal=animals[animal],
+        day=day,
+        tetrode_number=tetrode_number
+    )
+    return join(
+        data_dir, animals[animal].directory, 'EEG', filename)
+
+
+def get_mark_indicator_dataframe(tetrode_index, animals):
+    time = get_trial_time(tetrode_index, animals)
+    mark_dataframe = (get_mark_dataframe(tetrode_index, animals)
+                      .loc[time.min():time.max()])
+    mark_dataframe.index = time[
+        find_closest_ind(time, mark_dataframe.index.values)]
+    return mark_dataframe.reindex(index=time, fill_value=np.nan)
+
+
 if __name__ == '__main__':
     sys.exit()
