43d2c0991260acef81bb28d7354931cabcc68939
edeno <edeno@bu.edu>
2017-03-16 11:33:56 -0400

    Collect analysis after all epoch jobs have run

---
+++ scripts/collect_analysis.py
@@ -0,0 +1,101 @@
+from itertools import combinations
+from sys import exit
+
+import pandas as pd
+
+from src.data_processing import (get_all_area_pair_info,
+                                 get_all_ripple_info,
+                                 get_all_tetrode_info,
+                                 get_all_tetrode_pair_info,
+                                 get_brain_area_pairs_canonical_coherence,
+                                 get_brain_area_pairs_coherence,
+                                 get_brain_area_pairs_group_delay,
+                                 get_brain_area_power)
+from src.parameters import (FREQUENCY_BANDS, MULTITAPER_PARAMETERS,
+                            RIPPLE_COVARIATES)
+
+RESULTS_FILE = 'results.h5'
+
+
+def main():
+    tetrode_info = get_all_tetrode_info()
+    tetrode_pair_info = get_all_tetrode_pair_info()
+    area_pair_info = get_all_area_pair_info()
+    ripple_info = get_all_ripple_info()
+    save_analysis(tetrode_info, tetrode_pair_info, area_pair_info,
+                  ripple_info)
+
+    animal_groups = zip(tetrode_info.groupby('animal'),
+                        tetrode_pair_info.groupby('animal'),
+                        area_pair_info.groupby('animal'))
+
+    for (animal_tetrode_info, animal_tetrode_pair_info,
+         animal_area_pair_info) in animal_groups:
+        save_analysis(animal_tetrode_info[1], animal_tetrode_pair_info[1],
+                      animal_area_pair_info[1], ripple_info,
+                      label=animal_tetrode_info[0])
+
+    epoch_groups = zip(tetrode_info.groupby('epoch'),
+                       tetrode_pair_info.groupby('epoch'),
+                       area_pair_info.groupby('epoch'))
+
+    for (epoch_tetrode_info, epoch_tetrode_pair_info,
+         epoch_area_pair_info) in epoch_groups:
+        save_analysis(epoch_tetrode_info[1], epoch_tetrode_pair_info[1],
+                      epoch_area_pair_info[1], ripple_info,
+                      label=epoch_tetrode_info[0])
+
+    with pd.HDFStore(RESULTS_FILE) as store:
+        store.put('/ripple_info', ripple_info)
+        store.put('/tetrode_info', tetrode_info)
+        store.put('/tetrode_pair_info', tetrode_pair_info)
+        store.put('/area_pair_info', area_pair_info)
+
+
+def save(analysis_function, args=None):
+    path = '/'.join(args[:-1])
+    with pd.HDFStore(RESULTS_FILE) as store:
+        store.put(path, analysis_function(*args))
+
+
+def save_mean(label, multitaper_parameter_name, covariate, level,
+              tetrode_info, tetrode_pair_info, area_pair_info):
+    save(get_brain_area_power, args=(
+        label, multitaper_parameter_name, covariate, level,
+        tetrode_info))
+    save(get_brain_area_pairs_coherence, args=(
+        label, multitaper_parameter_name, covariate, level,
+        tetrode_pair_info))
+    save(get_brain_area_pairs_canonical_coherence, args=(
+        label, multitaper_parameter_name, covariate, level,
+        area_pair_info))
+    for frequency_band in FREQUENCY_BANDS:
+        save(get_brain_area_pairs_group_delay, args=(
+            label, multitaper_parameter_name, covariate, level,
+            frequency_band, tetrode_pair_info))
+
+
+def save_analysis(tetrode_info, tetrode_pair_info, area_pair_info,
+                  ripple_info, label=''):
+    RIPPLE_LOCKED_LEVELS = ['baseline', 'ripple_locked',
+                            'ripple_difference_from_baseline']
+    for multitaper_parameter_name in MULTITAPER_PARAMETERS:
+        for level in RIPPLE_LOCKED_LEVELS:
+            save_mean(label, multitaper_parameter_name, 'all_ripples',
+                      level, tetrode_info, tetrode_pair_info,
+                      area_pair_info)
+    for multitaper_parameter_name in MULTITAPER_PARAMETERS:
+        for covariate in RIPPLE_COVARIATES:
+            for level in ripple_info[covariate].unique().tolist():
+                save_mean(label, multitaper_parameter_name, covariate,
+                          level, tetrode_info, tetrode_pair_info,
+                          area_pair_info)
+            for levels in combinations(
+                    sorted(ripple_info[covariate].unique().tolist()), 2):
+                level = '_'.join(levels)
+                save_mean(label, multitaper_parameter_name, covariate,
+                          level, tetrode_info, tetrode_pair_info,
+                          area_pair_info)
+
+if __name__ == '__main__':
+    exit(main())
--- scripts/queue_cluster_jobs.py
+++ scripts/queue_cluster_jobs.py
@@ -9,16 +9,25 @@ from src.data_processing import make_epochs_dataframe
 from src.parameters import ANIMALS, N_DAYS
 
 
+def queue_job(python_cmd, directives=None, log_file='log.log',
+              job_name='job'):
+    'echo python {python_cmd}'.format(python_cmd=python_cmd)
+    queue_cmd = (
+        'qsub {directives} -j y -o {log_file} -N {job_name}').format(
+            directives=directives,
+            log_file=log_file,
+            job_name=job_name)
+    cmd_line_script = ' | '.join([python_cmd, queue_cmd])
+    run(cmd_line_script, shell=True)
+
+
 def main():
     log_directory = join(getcwd(), 'logs')
     makedirs(log_directory,  exist_ok=True)
 
     python_function = 'run_by_epoch.py'
-    directives = ('-l h_rt=6:00:00 '
-                  '-pe omp 12 '
-                  '-P braincom '
-                  '-notify '
-                  '-l mem_per_core=3G')
+    directives = ' '.join(['-l h_rt=6:00:00', '-pe omp 12', '-P braincom',
+                           '-notify', '-l mem_per_core=3G'])
 
     epoch_info = make_epochs_dataframe(ANIMALS, range(1, N_DAYS + 1))
     epoch_keys = epoch_info[(epoch_info.type == 'run') & (
@@ -29,20 +38,29 @@ def main():
         animal, day, epoch_ind = epoch_key
         log_file = '{animal}_{day:02d}_{epoch:02d}.log'.format(
             animal=animal, day=day, epoch=epoch_ind)
-        function_name = '{function_name}_{animal}_{day:02d}_{epoch:02d}'.format(
+        job_name = (
+            '{function_name}_{animal}_{day:02d}_{epoch:02d}').format(
             animal=animal, day=day, epoch=epoch_ind,
             function_name=python_function.replace('.py', ''))
-        python_cmd = 'echo python {python_function} {animal} {day} {epoch}'.format(
-            python_function=python_function,
-            animal=animal,
-            day=day,
+        python_cmd = '{python_function} {animal} {day} {epoch}'.format(
+            python_function=python_function, animal=animal, day=day,
             epoch=epoch_ind)
-        queue_cmd = 'qsub {directives} -j y -o {log_file} -N {function_name}'.format(
-            directives=directives,
-            log_file=join(log_directory, log_file),
-            function_name=function_name)
-        script = ' | '.join([python_cmd, queue_cmd])
-        run(script, shell=True)
+        queue_job(python_cmd,
+                  directives=directives,
+                  log_file=join(log_directory, log_file),
+                  job_name=job_name)
+
+    # Collect analysis after all epoch jobs have run
+    analysis_directives = ' '.join(
+        ['-pe omp 4', '-l h_rt=2:00:00', '-P braincom',
+         '-l mem_per_core=2G',
+         '-hold_jid "{epoch_function}*"'.format(
+             epoch_function=python_function.replace('.py', ''))
+         ])
+    queue_job('collect_analysis.py',
+              directives=analysis_directives,
+              log_file=join(log_directory, 'collect_analysis.log'),
+              job_name='collect_analysis')
 
 
 if __name__ == '__main__':
