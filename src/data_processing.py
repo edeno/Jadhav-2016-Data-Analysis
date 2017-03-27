a1d8ac99ab7dfdd32543cd28061f31971a458fc1
edeno <edeno@bu.edu>
2017-03-27 16:06:03 -0400

    Minor formatting

--- src/data_processing.py
+++ src/data_processing.py
@@ -1003,6 +1003,7 @@ def find_power_spectrum_from_pair_key(target_tetrode,
     power_spectrum_name = 'power_spectrum{}'.format(
         (tetrode1, tetrode2).index(target_tetrode) + 1)
     return pd.DataFrame(coherence_df[power_spectrum_name].rename('power'))
+        coherence_df[power_spectrum_name].rename('power'))
 
 
 def get_area_pair_group_from_hdf(multitaper_parameter_name, covariate,
