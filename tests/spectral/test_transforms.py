f937502358568defed64701cd4340c037fb87b58
edeno <edeno@bu.edu>
2017-05-24 15:59:29 -0400

    Add test for _add_trial_axis

---
+++ tests/spectral/test_transforms.py
@@ -0,0 +1,20 @@
+import numpy as np
+from pytest import mark
+
+from src.spectral.transforms import (_add_trial_axis, _sliding_window,
+                                     _nextpower2)
+
+
+def test__add_trial_axis():
+    n_time_samples, n_signals = (2, 3)
+    test_data = np.ones(
+        (n_time_samples, n_signals))
+    expected_shape = (n_time_samples, 1, n_signals)
+    assert np.allclose(_add_trial_axis(test_data).shape, expected_shape)
+
+    # if there is a trial dimension, do nothing
+    n_trials = 10
+    test_data = np.ones(
+        (n_time_samples, n_trials, n_signals))
+    expected_shape = (n_time_samples, n_trials, n_signals)
+    assert np.allclose(_add_trial_axis(test_data).shape, expected_shape)
