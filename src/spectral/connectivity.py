Fix noise covariance

src/spectral/connectivity.py
@@ -823,7 +823,7 @@ def _remove_instantaneous_causality(noise_covariance):
     '''
     variance = np.diagonal(noise_covariance, axis1=-1,
                            axis2=-2)[..., np.newaxis]
-    return (variance - noise_covariance ** 2 / variance.swapaxes(-1, -2))
+    return (variance.swapaxes(-1, -2) - noise_covariance ** 2 / variance)
 
 
 def _set_diagonal_to_zero(x):

tests/spectral/test_connectivity.py
@@ -503,14 +503,14 @@ def test__remove_instantaneous_causality():
     expected_rotated_noise_covariance = np.zeros((2, 2, 2))
 
     expected_rotated_noise_covariance[0, 0, 1] = (
-        x1[0, 0] - (x1[0, 1] ** 2 / x1[1, 1]))
-    expected_rotated_noise_covariance[0, 1, 0] = (
         x1[1, 1] - (x1[0, 1] ** 2 / x1[0, 0]))
+    expected_rotated_noise_covariance[0, 1, 0] = (
+        x1[0, 0] - (x1[1, 0] ** 2 / x1[1, 1]))
 
     expected_rotated_noise_covariance[1, 0, 1] = (
-        x2[0, 0] - (x2[0, 1] ** 2 / x2[1, 1]))
-    expected_rotated_noise_covariance[1, 1, 0] = (
         x2[1, 1] - (x2[0, 1] ** 2 / x2[0, 0]))
+    expected_rotated_noise_covariance[1, 1, 0] = (
+        x2[0, 0] - (x2[1, 0] ** 2 / x2[1, 1]))
 
     assert np.allclose(
         _remove_instantaneous_causality(noise_covariance),