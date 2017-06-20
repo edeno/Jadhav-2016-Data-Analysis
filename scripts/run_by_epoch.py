ccf3d559e77586f807d3054d15901ba6cd744665
edeno <edeno@bu.edu>
2017-06-20 16:49:28 -0400

    Add new connectivity functions

--- scripts/run_by_epoch.py
+++ scripts/run_by_epoch.py
@@ -6,21 +6,15 @@ from signal import SIGUSR1, SIGUSR2, signal
 from subprocess import PIPE, run
 from sys import exit, stdout
 
-from src.analysis import (canonical_coherence_by_ripple_type,
-                          coherence_by_ripple_type,
-                          decode_ripple_clusterless,
-                          detect_epoch_ripples, group_delay_by_ripple_type,
-                          ripple_triggered_canonical_coherence,
-                          ripple_triggered_coherence,
-                          ripple_triggered_group_delay, is_overlap)
+from src.analysis import (decode_ripple_clusterless,
+                          detect_epoch_ripples,
+                          ripple_triggered_connectivity,
+                          connectivity_by_ripple_type)
 from src.data_processing import (get_LFP_dataframe, make_tetrode_dataframe,
-                                 make_tetrode_pair_info,
-                                 save_multitaper_parameters,
-                                 save_ripple_info,
-                                 save_tetrode_info, save_tetrode_pair_info)
+                                 save_ripple_info)
 from src.parameters import (ANIMALS, SAMPLING_FREQUENCY,
                             MULTITAPER_PARAMETERS, FREQUENCY_BANDS,
-                            RIPPLE_COVARIATES, ALPHA)
+                            RIPPLE_COVARIATES)
 
 
 def estimate_ripple_coherence(epoch_key):
@@ -30,56 +24,30 @@ def estimate_ripple_coherence(epoch_key):
     tetrode_info = make_tetrode_dataframe(ANIMALS)[epoch_key]
     tetrode_info = tetrode_info[
         ~tetrode_info.descrip.str.endswith('Ref').fillna(False)]
-    save_tetrode_info(epoch_key, tetrode_info)
-
-    tetrode_pair_info = make_tetrode_pair_info(tetrode_info)
-    save_tetrode_pair_info(epoch_key, tetrode_pair_info)
 
     lfps = {tetrode_key: get_LFP_dataframe(tetrode_key, ANIMALS)
             for tetrode_key in tetrode_info.index}
 
-    # Compare before ripple to after ripple
+    # Compare all ripples
     for parameters_name, parameters in MULTITAPER_PARAMETERS.items():
-        ripple_triggered_coherence(
-            lfps, ripple_times,
+        ripple_triggered_connectivity(
+            lfps, epoch_key, tetrode_info, ripple_times, parameters,
+            FREQUENCY_BANDS,
             multitaper_parameter_name=parameters_name,
-            multitaper_params=parameters)
-        ripple_triggered_canonical_coherence(
-            lfps, epoch_key, tetrode_info, ripple_times,
-            multitaper_parameter_name=parameters_name,
-            multitaper_params=parameters)
-        for frequency_band_name, frequency_band in FREQUENCY_BANDS.items():
-            if is_overlap(
-                    frequency_band, parameters['desired_frequencies']):
-                ripple_triggered_group_delay(
-                    tetrode_pair_info, parameters_name, frequency_band,
-                    frequency_band_name, alpha=ALPHA)
-        save_multitaper_parameters(
-            epoch_key, parameters_name, parameters)
+            group_name='all_ripples')
 
     # Compare different types of ripples
     ripple_info = decode_ripple_clusterless(
         epoch_key, ANIMALS, ripple_times)[0]
-    save_ripple_info(epoch_key, ripple_info)
 
     for covariate in RIPPLE_COVARIATES:
         for parameters_name, parameters in MULTITAPER_PARAMETERS.items():
-            coherence_by_ripple_type(
-                lfps, ripple_info, covariate,
-                multitaper_parameter_name=parameters_name,
-                multitaper_params=parameters)
-            canonical_coherence_by_ripple_type(
+            connectivity_by_ripple_type(
                 lfps, epoch_key, tetrode_info, ripple_info, covariate,
-                multitaper_parameter_name=parameters_name,
-                multitaper_params=parameters)
-            for (frequency_band_name,
-                 frequency_band) in FREQUENCY_BANDS.items():
-                if is_overlap(
-                        frequency_band, parameters['desired_frequencies']):
-                    group_delay_by_ripple_type(
-                        tetrode_pair_info, ripple_info, covariate,
-                        parameters_name, frequency_band,
-                        frequency_band_name, alpha=ALPHA)
+                parameters, FREQUENCY_BANDS,
+                multitaper_parameter_name=parameters_name)
+
+    save_ripple_info(epoch_key, ripple_info.reset_index())
 
 
 def get_command_line_arguments():
