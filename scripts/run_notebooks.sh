617785905fb81c6028b0fde1c3e356280441a1e6
edeno <edeno@bu.edu>
2017-07-03 16:35:43 -0400

    Remove comment.

--- scripts/run_notebooks.sh
+++ scripts/run_notebooks.sh
@@ -6,6 +6,5 @@ FILES="../notebooks/2017_07_03_*.ipynb"
 for notebook in $FILES
 do
   echo "Processing $notebook file..."
-  # take action on each file. $f store current file name
   jupyter nbconvert --to notebook $notebook --output $notebook
 done
