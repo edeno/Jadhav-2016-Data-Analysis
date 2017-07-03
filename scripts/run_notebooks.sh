eef8b69a64fc2fff4f28eab41663f11fbc979ebc
edeno <edeno@bu.edu>
2017-07-03 16:49:53 -0400

    Add example usage

--- scripts/run_notebooks.sh
+++ scripts/run_notebooks.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+# Examples
+# --------
+# bash run_notebooks.sh '../notebooks/2017_07_03_Group_Delay_Analysis.ipynb' '../notebooks/2017_07_03_Canonical_Coherence.ipynb'
+# bash run_notebooks.sh "../notebooks/2017_07_03_*.ipynb"
 shopt -s nullglob
 FILES=($@)
 
