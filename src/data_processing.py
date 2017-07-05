2b6b0c3e1292aa8882fec1f83634dd218ac807ad
Eric Denovellis <edeno@bu.edu>
2017-07-05 09:51:28 -0400

    Use OSError instead of KeyError

--- src/data_processing.py
+++ src/data_processing.py
@@ -790,7 +790,7 @@ def read_netcdfs(files, dim, transform_func=None, group=None):
                 # can use it after closing each original file
                 ds.load()
                 return ds
-        except (IndexError, KeyError):
+        except (IndexError, OSError):
             logger.debug('Selection not found. \n'
                          'Path: {path} \n\t group: {group}'.format(
                             path=path, group=group))
