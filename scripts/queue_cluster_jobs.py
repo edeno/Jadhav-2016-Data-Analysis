e925d5ef103941e3f15e96edffdd063909f8ba98
edeno <edeno@bu.edu>
2017-08-08 09:56:28 -0400

    Increase wall time because pairwise spectral granger is taking longer

--- scripts/queue_cluster_jobs.py
+++ scripts/queue_cluster_jobs.py
@@ -41,7 +41,7 @@ def main():
 
     python_function = 'run_by_epoch.py'
     directives = ' '.join(
-        ['-l h_rt=1:30:00', '-pe omp {0}'.format(NUM_THREADS),
+        ['-l h_rt=3:00:00', '-pe omp {0}'.format(NUM_THREADS),
          '-P braincom', '-notify', '-l mem_total=125G',
          '-v OPENBLAS_NUM_THREADS'])
 
