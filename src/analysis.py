ef8e672f1e9fc9014b61b8855c625dd4251a4950
edeno <edeno@bu.edu>
2017-08-17 23:37:22 -0400

    Exclude reference tetrodes from clusterless decoding

--- src/analysis.py
+++ src/analysis.py
@@ -445,7 +445,10 @@ def decode_ripple_clusterless(epoch_key, animals, ripple_times,
                    drop=False))
     # Get cell-layer CA1, iCA1 LFPs
     is_hippocampal = tetrode_info.area.isin(['CA1', 'iCA1', 'CA3'])
-    hippocampal_tetrodes = tetrode_info[is_hippocampal]
+    hippocampal_tetrodes = tetrode_info[
+        is_hippocampal &
+        ~tetrode_info.descrip.str.endswith('Ref').dropna() &
+        ~tetrode_info.descrip.str.startswith('Ref').dropna())]
     logger.debug(hippocampal_tetrodes.loc[:, ['area', 'depth', 'descrip']])
 
     position_info = get_interpolated_position_dataframe(epoch_key, animals)
