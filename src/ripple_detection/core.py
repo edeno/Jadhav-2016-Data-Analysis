eddc1d33d079baab56a65c679df8f768509ba84a
edeno <edeno@bu.edu>
2017-08-30 12:09:03 -0400

    Remove unused functions

--- src/ripple_detection/core.py
+++ src/ripple_detection/core.py
@@ -38,178 +38,18 @@ def segment_boolean_series(series, minimum_duration=0.015):
             if end_time >= (start_time + minimum_duration)]
 
 
-def multitaper_Kay_method(lfps, minimum_duration=0.015,
-                          sampling_frequency=1500, zscore_threshold=2,
-                          multitaper_kwargs={}):
-    '''Uses the multitaper ripple-band power from each tetrode and combines
-    using
-    '''
-    m = Multitaper(
-        pd.Panel(lfps).values.squeeze().T,
-        sampling_frequency=sampling_frequency,
-        time_halfbandwidth_product=1,
-        time_window_duration=0.020,
-        time_window_step=0.020,
-        start_time=0)
-    c = Connectivity.from_multitaper(m)
-    ripple_band_index = (c.frequencies >= 150) & (c.frequencies <= 250)
-    ripple_power = np.mean(c.power()[..., ripple_band_index, :], axis=-2)
-    return _get_candidate_ripples_Kay(
-        ripple_power, is_multitaper=True,
-        zscore_threshold=zscore_threshold,
-        minimum_duration=minimum_duration)
-
-
-def mulititaper_Karlsson_method(lfps, minimum_duration=0.015,
-                                sampling_frequency=1500,
-                                zscore_threshold=2, multitaper_kwargs={}):
-    m = Multitaper(
-        pd.Panel(lfps).values.squeeze().T,
-        sampling_frequency=sampling_frequency,
-        time_halfbandwidth_product=1,
-        time_window_duration=0.020,
-        time_window_step=0.020,
-        start_time=0)
-    c = Connectivity.from_multitaper(m)
-    ripple_band_index = (c.frequencies >= 150) & (c.frequencies <= 250)
-    ripple_power = np.mean(c.power()[..., ripple_band_index, :], axis=-2)
-    return _get_candidate_ripples_Karlsson(
-        ripple_power, minimum_duration=minimum_duration,
-        zscore_threshold=zscore_threshold)
-
-
-def Kay_method(lfps, minimum_duration=0.015, zscore_threshold=2,
-               smoothing_sigma=0.004, sampling_frequency=1500):
-    filtered_lfps = [pd.Series(
-        _ripple_bandpass_filter(lfp.values.ravel()), index=lfp.index)
-        for lfp in lfps]
-    return _get_candidate_ripples_Kay(
-        filtered_lfps, is_multitaper=False,
-        minimum_duration=minimum_duration,
-        zscore_threshold=zscore_threshold, sigma=smoothing_sigma,
-        sampling_frequency=sampling_frequency)
-
-
-def Karlsson_method(lfps, smoothing_sigma=0.004, sampling_frequency=1500,
-                    minimum_duration=0.015, zscore_threshold=2):
-    ripple_envelope = [_get_smoothed_envelope(lfp, smoothing_sigma,
-                                              sampling_frequency)
-                       for lfp in lfps]
-    return _get_candidate_ripples_Karlsson(
-        ripple_envelope, minimum_duration=minimum_duration,
-        zscore_threshold=zscore_threshold)
-
-
-def _get_smoothed_envelope(lfp, sigma, sampling_frequency):
-    '''Filters the lfp between 150-250 Hz and returns the
-    smoothed envelope of the filtered signal
-    '''
-    return pd.Series(_smooth(_get_envelope(_ripple_bandpass_filter(
-        lfp.values.flatten())), sigma, sampling_frequency),
-        index=lfp.index)
-
-
-def _get_candidate_ripples_Kay(filtered_lfps, is_multitaper=False,
-                               minimum_duration=0.015,
-                               zscore_threshold=2, sigma=0.004,
-                               sampling_frequency=1500):
-    '''Candidate ripple times are extracted based on mean power on all
-    tetrodes
-
-    Using power from all tetrodes reflects that we view the ripple as a
-    population event
-
-    Parameters
-    ----------
-    filtered_lfps : Pandas series or array_like
-        Time series of ripple band power
-    is_multitaper : bool
-        Are we using multi-taper methods to extract the power?
-    minimum_duration : float, optional
-        Minimum time the z-score has to stay above threshold to be
-        considered a candidate ripple
-    zscore_threshold : float, optional
-        Number of standard deviations the ripple power must exceed to be
-        considered a candidate ripple
-    sigma : float, optional
-    sampling_frequency : int, optional
-
-    Returns
-    -------
-    ripple_times : list of 2-element tuples
-        The elements correspond to the start and stop times of the ripple
-
-    References
-    ----------
-    .. [1] Kay, K., Sosa, M., Chung, J.E., Karlsson, M.P., Larkin, M.C.,
-    and Frank, L.M. (2016). A hippocampal network for spatial coding during
-    immobility and sleep. Nature 531, 185-190.
-
-    '''
-    combined_lfps = _squared_sum(pd.concat(filtered_lfps, axis=1), axis=1)
-
-    smooth_combined_lfps = combined_lfps if is_multitaper else pd.Series(
-        _smooth(combined_lfps.values.flatten(), sigma, sampling_frequency),
-        index=combined_lfps.index)
-
-    thresholded_combined_lfps = _threshold_by_zscore(
-        np.sqrt(smooth_combined_lfps), zscore_threshold=zscore_threshold)
-    return list(sorted(_extend_threshold_to_mean(
-        thresholded_combined_lfps.is_above_mean,
-        thresholded_combined_lfps.is_above_threshold,
-        minimum_duration=minimum_duration)))
-
-
-def _squared_sum(data, axis=1):
-    return np.sum(data, axis=axis) ** 2
-
-
-def _get_candidate_ripples_Karlsson(filtered_lfps, minimum_duration=0.015,
-                                    zscore_threshold=2):
-    '''Candidate ripple times are detected on each tetrode and then
-    combined if they overlap
-
-    Parameters
-    ----------
-    filtered_lfps : Pandas series or array_like
-        Time series of ripple band power
-    minimum_duration : float, optional
-        Minimum time the z-score has to stay above threshold to be
-        considered a candidate ripple
-    zscore_threshold : float, optional
-        Number of standard deviations the ripple power must exceed to be
-        considered a candidate ripple
-
-    Returns
-    -------
-    ripple_times : list of 2-element tuples
-        The elements correspond to the start and stop times of the ripple
-
-    References
-    ----------
-    .. [1] Karlsson, M.P., and Frank, L.M. (2009). Awake replay of remote
-    experiences in the hippocampus. Nature Neuroscience 12, 913-918.
-
-    '''
-    thresholded_lfps = [_threshold_by_zscore(
-        lfp, zscore_threshold=zscore_threshold)
-        for lfp in filtered_lfps]
-    candidate_ripple_times = [_extend_threshold_to_mean(
-        lfp.is_above_mean, lfp.is_above_threshold,
-        minimum_duration=minimum_duration)
-        for lfp in thresholded_lfps]
-    return list(_merge_overlapping_ranges(
-        _flatten_list(candidate_ripple_times)))
-
-
-def _flatten_list(original_list):
-    '''Takes a list of lists and turns it into a single list'''
-    return [item for sublist in original_list for item in sublist]
-
-
-def _ripple_bandpass_filter(data):
+def ripple_bandpass_filter(data):
     '''Returns a bandpass filtered signal between 150-250 Hz using the
-    Frank lab filter
+    Frank lab filter.
+
+    Parameters
+    ----------
+    data : array_like, shape (n_time,)
+
+    Returns
+    -------
+    filtered_data : array_like, shape (n_time,)
+
     '''
     filter_numerator, filter_denominator = _get_ripplefilter_kernel()
     is_nan = np.isnan(data)
@@ -330,18 +170,12 @@ def _threshold_by_zscore(data, zscore_threshold=2):
 
     Parameters
     ----------
-    data : array_like or Pandas series
+    data : array_like, shape (n_time,)
     zscore_threshold : int, optional
 
     Returns
     -------
-    threshold_dataframe : Pandas dataframe
-        Dataframe contains two columns. One column is an indicator function
-        where 1 indicates the z-score is above the mean (z-score = 0) and 0
-        indicates the z-score is below the mean. The other column is an
-        indicator function of z-scored data where 1 indicates the z-score
-        is above the threshold parameter and 0 indicates the z-score is
-        below the threshold parameter.
+    candidate_ripple_times : pandas Dataframe
 
     '''
     zscored_data = zscore(data)
