423b9e20446867be0d5820defcd06e146ec7d9f5
edeno <edeno@bu.edu>
2017-08-31 13:15:05 -0400

    Use more specific names

--- src/ripple_decoding/decoders.py
+++ src/ripple_decoding/decoders.py
@@ -1,20 +1,21 @@
 class ClusterlessDecoder(object):
-    def __init__(self):
+    def __init__(self, n_place_bins=61, mark_std_deviation=20):
+        self.n_place_bins = n_place_bins
+        self.mark_std_deviation = mark_std_deviation
+
+    def fit(self, position, spike_marks, states):
         pass
 
-    def fit(self, X, Y):
-        pass
-
-    def predict(self, X):
+    def predict(self, spike_marks):
         pass
 
 
-class SpikeDecoder(object):
-    def __init__(self):
+class SortedSpikeDecoder(object):
+    def __init__(self, n_place_bins=61):
+        self.n_place_bins = n_place_bins
+
+    def fit(self, position, spikes, states):
         pass
 
-    def fit(self, X, Y):
-        pass
-
-    def predict(self, X):
+    def predict(self, spikes):
         pass
