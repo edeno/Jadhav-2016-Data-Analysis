747d66bb39580c47b59a4d59736093b0add448e6
edeno <edeno@bu.edu>
2017-08-31 12:49:57 -0400

    Stub out classes and modules

---
+++ src/ripple_decoding/decoders.py
@@ -0,0 +1,20 @@
+class ClusterlessDecoder(object):
+    def __init__(self):
+        pass
+
+    def fit(self, X, Y):
+        pass
+
+    def predict(self, X):
+        pass
+
+
+class SpikeDecoder(object):
+    def __init__(self):
+        pass
+
+    def fit(self, X, Y):
+        pass
+
+    def predict(self, X):
+        pass
