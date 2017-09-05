3f69cf863891291e6f0e6352367c15ae6d188d2f
edeno <edeno@bu.edu>
2017-09-04 22:44:20 -0400

    Flesh out fit function

--- src/ripple_decoding/decoders.py
+++ src/ripple_decoding/decoders.py
@@ -1,3 +1,16 @@
+import numpy as np
+from logging import getLogger
+
+from .clusterless import (build_joint_mark_intensity,
+                          estimate_ground_process_intensity,
+                          poisson_mark_likelihood)
+from .core import (combined_likelihood,
+                   empirical_movement_transition_matrix,
+                   get_bin_centers, predict_state, set_initial_conditions)
+
+logger = getLogger(__name__)
+
+
 class ClusterlessDecoder(object):
     '''
 
@@ -8,23 +21,98 @@ class ClusterlessDecoder(object):
 
     '''
 
-    def __init__(self, n_position_bins=61, mark_std_deviation=20):
+    def __init__(self, n_position_bins=61, mark_std_deviation=20,
+                 sequence_compression_factor=16):
         self.n_position_bins = n_position_bins
         self.mark_std_deviation = mark_std_deviation
+        self.sequence_compression_factor = sequence_compression_factor
+        self.posterior_density = []
 
-    def fit(self, position, spike_marks, states):
-        '''Fits the decoder model by state.
+    def fit(self, position, trajectory_direction, spike_marks):
+        '''Fits the decoder model for each trajectory_direction.
 
-        Relates the position and spike_marks to the state.
+        Relates the position and spike_marks to the trajectory_direction.
 
         Parameters
         ----------
-        position : array_like, shape (n_time,)
-        spike_marks : array_like, shape (n_time, n_marks)
+        position : ndarray, shape (n_time,)
+        trajectory_direction : array_like, shape (n_time,)
+            Task at the current time. Element must be either
+             'Inbound' or 'Outbound'.
+        spike_marks : ndarray, shape (n_time, n_marks, n_signals)
             If spike does not occur, the row must be marked with np.nan.
-        states : array_like, shape (n_time,)
+
+        Returns
+        -------
+        self : class instance
 
         '''
+
+        trajectory_direction = np.array(trajectory_direction)
+
+        self.place_bin_edges = np.linspace(
+            np.floor(position.min()), np.ceil(position.max()),
+            self.n_position_bins + 1)
+        self.place_std_deviation = np.diff(self.place_bin_edges)[0]
+        self.place_bin_centers = get_bin_centers(self.place_bin_edges)
+
+        LIKELIHOOD_STATE_ORDER = ['Outbound', 'Outbound',
+                                  'Inbound', 'Inbound']
+
+        initial_conditions = set_initial_conditions(
+            self.place_bin_edges, self.place_bin_centers)
+        self.initial_conditions = np.stack(
+            [initial_conditions[state] for state in LIKELIHOOD_STATE_ORDER]
+        ) * 0.25
+
+        trajectory_directions = np.unique(trajectory_direction)
+
+        logger.info('Fitting state transitions...')
+        STATE_TRANSITION_ORDER = ['Outbound', 'Inbound',
+                                  'Inbound', 'Outbound']
+
+        state_transition_by_state = {
+            direction: empirical_movement_transition_matrix(
+                position[np.in1d(trajectory_direction, direction)],
+                self.place_bin_edges, self.sequence_compression_factor)
+            for direction in trajectory_directions}
+        self.state_transition_matrix = np.stack(
+            [state_transition_by_state[state]
+             for state in STATE_TRANSITION_ORDER])
+
+        logger.info('Fitting likelihood model...')
+        joint_mark_intensity_functions = []
+        ground_process_intensity = []
+
+        for marks in spike_marks:
+            jmi_by_state = {
+                direction: build_joint_mark_intensity(
+                    position[np.in1d(trajectory_direction, direction)],
+                    marks[np.in1d(trajectory_direction, direction)],
+                    self.place_bin_centers, self.place_std_deviation,
+                    self.mark_std_deviation)
+                for direction in trajectory_directions}
+            joint_mark_intensity_functions.append(
+                [jmi_by_state[state] for state in LIKELIHOOD_STATE_ORDER])
+
+            gpi = {
+                direction: estimate_ground_process_intensity(
+                    position[np.in1d(trajectory_direction, direction)],
+                    marks[np.in1d(trajectory_direction, direction)],
+                    self.place_bin_centers, self.place_std_deviation)
+                for direction in trajectory_directions}
+            ground_process_intensity.append(
+                [gpi[state] for state in LIKELIHOOD_STATE_ORDER])
+
+        ground_process_intensity = np.stack(ground_process_intensity)
+        likelihood_kwargs = dict(
+            joint_mark_intensity_functions=joint_mark_intensity_functions,
+            ground_process_intensity=ground_process_intensity)
+
+        self._combined_likelihood_kwargs = dict(
+            likelihood_function=poisson_mark_likelihood,
+            likelihood_kwargs=likelihood_kwargs)
+
         return self
 
     def predict(self, spike_marks):
@@ -32,7 +120,7 @@ class ClusterlessDecoder(object):
 
         Parameters
         ----------
-        spike_marks : array_like, shape (n_time, n_marks)
+        spike_marks : ndarray, shape (n_time, n_marks)
             If spike does not occur, the row must be marked with np.nan.
 
         Returns
@@ -40,7 +128,13 @@ class ClusterlessDecoder(object):
         predicted_state : str
 
         '''
-        pass
+        self.posterior_density.append(
+            predict_state(
+                spike_marks,
+                initial_conditions=self.initial_conditions,
+                state_transition=self.state_transition_matrix,
+                likelihood_function=combined_likelihood,
+                likelihood_kwargs=self._combined_likelihood_kwargs))
 
 
 class SortedSpikeDecoder(object):
@@ -55,16 +149,16 @@ class SortedSpikeDecoder(object):
         '''
         self.n_position_bins = n_position_bins
 
-    def fit(self, position, spikes, states):
+    def fit(self, position, spikes, trajectory_direction):
         '''Fits the decoder model by state
 
         Relates the position and spikes to the state.
 
         Parameters
         ----------
-        position : array_like, shape (n_time,)
-        spike : array_like, shape (n_time,)
-        states : array_like, shape (n_time,)
+        position : ndarray, shape (n_time,)
+        spike : ndarray, shape (n_time,)
+        trajectory_direction : ndarray, shape (n_time,)
 
         '''
         return self
@@ -74,7 +168,7 @@ class SortedSpikeDecoder(object):
 
         Parameters
         ----------
-        spike : array_like, shape (n_time,)
+        spike : ndarray, shape (n_time,)
 
         Returns
         -------
