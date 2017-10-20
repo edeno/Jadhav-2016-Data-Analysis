003c23be2aae70cc793d3e3dd7692dbaf6392d8c
Eric Denovellis <edeno@bu.edu>
2017-10-20 08:09:02 -0700

    Fix import

--- src/data_processing/__init__.py
+++ src/data_processing/__init__.py
@@ -1,7 +1,8 @@
 # flake8: noqa
 from .multiunit import get_mark_dataframe, get_mark_indicator_dataframe
 from .neurons import make_neuron_dataframe, get_spikes_dataframe, get_spike_indicator_dataframe
-from .position import get_position_dataframe, get_interpolated_position_dataframe, get_computed_ripples_dataframe, get_computed_consensus_ripple_times
+from .position import get_position_dataframe, get_interpolated_position_dataframe
+from .ripples import get_computed_ripples_dataframe, get_computed_consensus_ripple_times
 from .saving import get_analysis_file_path, save_xarray, open_mfdataset, read_analysis_files
 from .task import make_epochs_dataframe
 from .tetrodes import get_LFP_dataframe, make_tetrode_dataframe, get_trial_time
