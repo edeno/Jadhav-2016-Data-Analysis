cf7bd1200d175e5c6c39b3627dfe92a2e00d7f3b
Eric Denovellis <edeno@bu.edu>
2017-11-01 10:54:55 -0400

    Now use full paths in the ANIMALS dictionary for the revised `loren_frank_data_processing` package

--- src/parameters.py
+++ src/parameters.py
@@ -1,12 +1,21 @@
 from collections import namedtuple
+from os.path import abspath, dirname, join, pardir
 
 SAMPLING_FREQUENCY = 1500
 
+# Data directories and definitions
+ROOT_DIR = join(abspath(dirname(__file__)), pardir)
+RAW_DATA_DIR = join(ROOT_DIR, 'Raw-Data')
+PROCESSED_DATA_DIR = join(ROOT_DIR, 'Processed-Data')
+
 Animal = namedtuple('Animal', {'directory', 'short_name'})
 ANIMALS = {
-    'HPa': Animal(directory='HPa_direct', short_name='HPa'),
-    'HPb': Animal(directory='HPb_direct', short_name='HPb'),
-    'HPc': Animal(directory='HPc_direct', short_name='HPc'),
+    'HPa': Animal(directory=join(RAW_DATA_DIR, 'HPa_direct'),
+                  short_name='HPa'),
+    'HPb': Animal(directory=join(RAW_DATA_DIR, 'HPb_direct'),
+                  short_name='HPb'),
+    'HPc': Animal(directory=join(RAW_DATA_DIR, 'HPc_direct'),
+                  short_name='HPc'),
 }
 
 
