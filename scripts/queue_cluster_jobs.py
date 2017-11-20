c12837140f40a9217553a515950d7472f35c593f
Eric Denovellis <edeno@bu.edu>
2017-11-19 19:45:54 -0500

    Lower wall time

--- scripts/queue_cluster_jobs.py
+++ scripts/queue_cluster_jobs.py
@@ -43,7 +43,7 @@ def main():
 
     python_function = 'run_by_epoch.py'
     directives = ' '.join(
-        ['-l h_rt=1:00:00', '-pe omp {0}'.format(NUM_THREADS),
+        ['-l h_rt=0:20:00', '-pe omp {0}'.format(NUM_THREADS),
          '-P braincom', '-notify', '-l mem_total=125G',
          '-v OPENBLAS_NUM_THREADS', '-v NUMBA_NUM_THREADS',
          '-v OMP_NUM_THREADS'])
